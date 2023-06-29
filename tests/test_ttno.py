0b42101cef9b88e85765961d1c7aa4e91e5c229e
Keefe Huang <keefe.huang@tum.de>
2023-06-29 18:04:12 +0200

    Update ttno tests to new interface

--- tests/test_ttno.py
+++ tests/test_ttno.py
@@ -35,96 +35,93 @@ class TestTTNOBasics(unittest.TestCase):
         reference_tree = ptn.TreeTensorNetwork()
         d = 2
 
-        node1 = ptn.random_tensor_node((d,d,d), identifier="id1")
-        node2 = ptn.random_tensor_node((d,d), identifier="id2")
-        node3 = ptn.random_tensor_node((d,d,d), identifier="id3")
-        node4 = ptn.random_tensor_node((d,d), identifier="id4")
+        node1, tensor1 = ptn.random_tensor_node((d, d, d), identifier="id1")
+        node2, tensor2 = ptn.random_tensor_node((d, d), identifier="id2")
+        node3, tensor3 = ptn.random_tensor_node((d, d, d), identifier="id3")
+        node4, tensor4 = ptn.random_tensor_node((d, d), identifier="id4")
 
-        reference_tree.add_root(node1)
-        reference_tree.add_child_to_parent(node2, 1, "id1", 2)
-        reference_tree.add_child_to_parent(node3, 1, "id1", 1)
-        reference_tree.add_child_to_parent(node4, 1, "id3", 2)
+        reference_tree.add_root(node1, tensor1)
+        reference_tree.add_child_to_parent(node2, tensor2, 1, "id1", 2)
+        reference_tree.add_child_to_parent(node3, tensor3,  1, "id1", 1)
+        reference_tree.add_child_to_parent(node4, tensor4,  1, "id3", 2)
 
-        shape = [2,3,4,5,6,7,8,9]
+        shape = [2, 3, 4, 5, 6, 7, 8, 9]
         start_tensor = ptn.crandn(shape)
         leg_dict = {"id1": 0, "id2": 1, "id3": 2, "id4": 3}
 
         ttno = ptn.TTNO.from_tensor(reference_tree, start_tensor, leg_dict)
 
-        correct_open_leg_dimensions = {"id1": (2,6), "id2": (3,7),
-                                        "id3": (4,8), "id4": (5,9)}
+        correct_open_leg_dimensions = {"id1": (2, 6), "id2": (3, 7),
+                                       "id3": (4, 8), "id4": (5, 9)}
 
         for node_id in correct_open_leg_dimensions:
             correct_shape = correct_open_leg_dimensions[node_id]
-            node = ttno.nodes[node_id]
-            found_shape = node.shape_of_legs(node.open_legs)
+            tensor = ttno.tensors[node_id]
+            found_shape = tensor.shape[-2:]
             self.assertEqual(correct_shape, found_shape)
 
         contracted_ttno = ttno.completely_contract_tree(to_copy=True)
-        contracted_tensor = contracted_ttno.nodes[contracted_ttno.root_id].tensor
+        contracted_tensor = contracted_ttno.tensors[contracted_ttno.root_id]
 
         # The shape is not retained throughout the entire procedure
-        correct_tensor = start_tensor.transpose((0,4,1,5,2,6,3,7))
+        correct_tensor = start_tensor.transpose((0, 4, 1, 5, 2, 6, 3, 7))
 
         self.assertTrue(np.allclose(correct_tensor, contracted_tensor))
 
     def test_from_tensorB(self):
         reference_tree = ptn.TreeTensorNetwork()
-        node1 = ptn.random_tensor_node((2,3,4,5), identifier="id1")
-        node2 = ptn.random_tensor_node((2,3,4,5), identifier="id2")
-        node3 = ptn.random_tensor_node((2,3,4,5), identifier="id3")
-        node4 = ptn.random_tensor_node((2,3,4,5), identifier="id4")
-        node5 = ptn.random_tensor_node((2,3,4,5), identifier="id5")
+        node1, tensor1 = ptn.random_tensor_node((2, 3, 4, 5), identifier="id1")
+        node2, tensor2 = ptn.random_tensor_node((2, 3, 4, 5), identifier="id2")
+        node3, tensor3 = ptn.random_tensor_node((2, 3, 4, 5), identifier="id3")
+        node4, tensor4 = ptn.random_tensor_node((2, 3, 4, 5), identifier="id4")
+        node5, tensor5 = ptn.random_tensor_node((2, 3, 4, 5), identifier="id5")
 
-        reference_tree.add_root(node1)
-        reference_tree.add_child_to_parent(node2, 0, "id1", 0)
-        reference_tree.add_child_to_parent(node3, 1, "id1", 1)
-        reference_tree.add_child_to_parent(node4, 2, "id3", 2)
-        reference_tree.add_child_to_parent(node5, 2, "id1", 2)
+        reference_tree.add_root(node1, tensor1)
+        reference_tree.add_child_to_parent(node2, tensor2, 0, "id1", 0)
+        reference_tree.add_child_to_parent(node3, tensor3, 1, "id1", 1)
+        reference_tree.add_child_to_parent(node4, tensor4, 2, "id3", 2)
+        reference_tree.add_child_to_parent(node5, tensor5, 2, "id1", 2)
 
         num_nodes = len(reference_tree.nodes)
 
-        shape = [i for i in range(2,(num_nodes + 2))]
+        shape = [i for i in range(2, (num_nodes + 2))]
         shape.extend(shape)
         start_tensor = ptn.crandn(shape)
         leg_dict = {"id" + str(i + 1): i for i in range(num_nodes)}
-
         ttno = ptn.TTNO.from_tensor(reference_tree, start_tensor, leg_dict)
 
         contracted_ttno = ttno.completely_contract_tree(to_copy=True)
-        contracted_tensor = contracted_ttno.nodes[contracted_ttno.root_id].tensor
-
+        contracted_tensor = contracted_ttno.tensors[contracted_ttno.root_id]
         # The shape is not retained throughout the entire procedure
         permutation = []
-        self._find_permutation(ttno, leg_dict, ttno.root_id, permutation)
-
+        self._find_permutation_rec(ttno, leg_dict, ttno.root_id, permutation)
         correct_tensor = start_tensor.transpose(permutation)
 
         self.assertTrue(np.allclose(correct_tensor, contracted_tensor))
 
     def test_from_tensorC(self):
         reference_tree = ptn.TreeTensorNetwork()
-        node1 = ptn.random_tensor_node((2,3,4,5), identifier="id1")
-        node2 = ptn.random_tensor_node((2,3,4,5), identifier="id2")
-        node3 = ptn.random_tensor_node((2,3,4,5), identifier="id3")
-        node4 = ptn.random_tensor_node((2,3,4,5), identifier="id4")
-        node5 = ptn.random_tensor_node((2,3,4,5), identifier="id5")
-        node6 = ptn.random_tensor_node((2,3,4,5), identifier="id6")
-        node7 = ptn.random_tensor_node((2,3,4,5), identifier="id7")
-        node8 = ptn.random_tensor_node((2,3,4,5), identifier="id8")
-        node9 = ptn.random_tensor_node((2,3,4,5), identifier="id9")
-        node10 = ptn.random_tensor_node((2,3,4,5), identifier="id10")
+        node1, tensor1 = ptn.random_tensor_node((2, 3, 4, 5), identifier="id1")
+        node2, tensor2 = ptn.random_tensor_node((2, 3, 4, 5), identifier="id2")
+        node3, tensor3 = ptn.random_tensor_node((2, 3, 4, 5), identifier="id3")
+        node4, tensor4 = ptn.random_tensor_node((2, 3, 4, 5), identifier="id4")
+        node5, tensor5 = ptn.random_tensor_node((2, 3, 4, 5), identifier="id5")
+        node6, tensor6 = ptn.random_tensor_node((2, 3, 4, 5), identifier="id6")
+        node7, tensor7 = ptn.random_tensor_node((2, 3, 4, 5), identifier="id7")
+        node8, tensor8 = ptn.random_tensor_node((2, 3, 4, 5), identifier="id8")
+        node9, tensor9 = ptn.random_tensor_node((2, 3, 4, 5), identifier="id9")
+        node10, tensor10 = ptn.random_tensor_node((2, 3, 4, 5), identifier="id10")
 
-        reference_tree.add_root(node1)
-        reference_tree.add_child_to_parent(node2, 0, "id1", 0)
-        reference_tree.add_child_to_parent(node3, 3, "id2", 3)
-        reference_tree.add_child_to_parent(node4, 0, "id3", 0)
-        reference_tree.add_child_to_parent(node5, 2, "id2", 2)
-        reference_tree.add_child_to_parent(node6, 0, "id5", 0)
-        reference_tree.add_child_to_parent(node7, 1, "id5", 1)
-        reference_tree.add_child_to_parent(node8, 1, "id1", 1)
-        reference_tree.add_child_to_parent(node9, 2, "id8", 2)
-        reference_tree.add_child_to_parent(node10, 2, "id1", 2)
+        reference_tree.add_root(node1, tensor1)
+        reference_tree.add_child_to_parent(node2, tensor2, 0, "id1", 0)
+        reference_tree.add_child_to_parent(node3, tensor3, 3, "id2", 3)
+        reference_tree.add_child_to_parent(node4, tensor4, 0, "id3", 0)
+        reference_tree.add_child_to_parent(node5, tensor5, 2, "id2", 2)
+        reference_tree.add_child_to_parent(node6, tensor6, 0, "id5", 0)
+        reference_tree.add_child_to_parent(node7, tensor7, 1, "id5", 1)
+        reference_tree.add_child_to_parent(node8, tensor8, 1, "id1", 1)
+        reference_tree.add_child_to_parent(node9, tensor9, 2, "id8", 2)
+        reference_tree.add_child_to_parent(node10, tensor10, 2, "id1", 2)
 
         d = 2
         num_nodes = len(reference_tree.nodes)
@@ -132,39 +129,39 @@ class TestTTNOBasics(unittest.TestCase):
         shape = [d for i in range(2 * num_nodes)]
         start_tensor = ptn.crandn(shape)
         leg_dict = {"id" + str(i + 1): i for i in range(num_nodes)}
-
         ttno = ptn.TTNO.from_tensor(reference_tree, start_tensor, leg_dict)
 
         contracted_ttno = ttno.completely_contract_tree(to_copy=True)
-        contracted_tensor = contracted_ttno.nodes[contracted_ttno.root_id].tensor
+        contracted_tensor = contracted_ttno.tensors[contracted_ttno.root_id]
 
         # The shape is not retained throughout the entire procedure
         permutation = []
-        self._find_permutation(ttno, leg_dict, ttno.root_id, permutation)
+        self._find_permutation_rec(ttno, leg_dict, ttno.root_id, permutation)
 
         correct_tensor = start_tensor.transpose(permutation)
 
         self.assertTrue(np.allclose(correct_tensor, contracted_tensor))
 
+
 class TestTTNOfromHamiltonian(unittest.TestCase):
     def setUp(self):
         self.ref_tree = ptn.TreeTensorNetwork()
 
-        node1 = ptn.TensorNode(ptn.crandn((2,2,2)), identifier="site1")
-        node2 = ptn.TensorNode(ptn.crandn((2,2,2,2)), identifier="site2")
-        node5 = ptn.TensorNode(ptn.crandn((2,2,2,2)), identifier="site5")
-        node3 = ptn.TensorNode(ptn.crandn((2,2)), identifier="site3")
-        node4 = ptn.TensorNode(ptn.crandn((2,2)), identifier="site4")
-        node6 = ptn.TensorNode(ptn.crandn((2,2)), identifier="site6")
-        node7 = ptn.TensorNode(ptn.crandn((2,2)), identifier="site7")
+        node1, tensor1 = ptn.random_tensor_node((2, 2, 2), identifier="site1")
+        node2, tensor2 = ptn.random_tensor_node((2, 2, 2, 2), identifier="site2")
+        node5, tensor5 = ptn.random_tensor_node((2, 2, 2, 2), identifier="site5")
+        node3, tensor3 = ptn.random_tensor_node((2, 2), identifier="site3")
+        node4, tensor4 = ptn.random_tensor_node((2, 2), identifier="site4")
+        node6, tensor6 = ptn.random_tensor_node((2, 2), identifier="site6")
+        node7, tensor7 = ptn.random_tensor_node((2, 2), identifier="site7")
 
-        self.ref_tree.add_root(node1)
-        self.ref_tree.add_child_to_parent(node2, 0, "site1", 0)
-        self.ref_tree.add_child_to_parent(node5, 0, "site1", 1)
-        self.ref_tree.add_child_to_parent(node3, 0, "site2", 1)
-        self.ref_tree.add_child_to_parent(node4, 0, "site2", 2)
-        self.ref_tree.add_child_to_parent(node6, 0, "site5", 1)
-        self.ref_tree.add_child_to_parent(node7, 0, "site5", 2)
+        self.ref_tree.add_root(node1, tensor1)
+        self.ref_tree.add_child_to_parent(node2, tensor2, 0, "site1", 0)
+        self.ref_tree.add_child_to_parent(node5, tensor5, 0, "site1", 1)
+        self.ref_tree.add_child_to_parent(node3, tensor3, 0, "site2", 1)
+        self.ref_tree.add_child_to_parent(node4, tensor4, 0, "site2", 2)
+        self.ref_tree.add_child_to_parent(node6, tensor6, 0, "site5", 1)
+        self.ref_tree.add_child_to_parent(node7, tensor7, 0, "site5", 2)
 
         self.term = term = {"site1": "1",
                             "site2": "2",
@@ -175,13 +172,13 @@ class TestTTNOfromHamiltonian(unittest.TestCase):
                             "site7": "7"}
 
     def test_from_hamiltonian_one_term(self):
-        conversion_dictionary = {"1": ptn.crandn((2,2)),
-                                 "2": ptn.crandn((2,2)),
-                                 "3": ptn.crandn((2,2)),
-                                 "4": ptn.crandn((2,2)),
-                                 "5": ptn.crandn((2,2)),
-                                 "6": ptn.crandn((2,2)),
-                                 "7": ptn.crandn((2,2)),
+        conversion_dictionary = {"1": ptn.crandn((2, 2)),
+                                 "2": ptn.crandn((2, 2)),
+                                 "3": ptn.crandn((2, 2)),
+                                 "4": ptn.crandn((2, 2)),
+                                 "5": ptn.crandn((2, 2)),
+                                 "6": ptn.crandn((2, 2)),
+                                 "7": ptn.crandn((2, 2)),
                                  }
         hamiltonian = ptn.Hamiltonian(terms=[self.term],
                                       conversion_dictionary=conversion_dictionary)
@@ -190,9 +187,11 @@ class TestTTNOfromHamiltonian(unittest.TestCase):
 
         for node_id in ttno.nodes:
             node = ttno.nodes[node_id]
+            tensor = ttno.tensors[node_id]
 
-            shape = node.tensor.shape
+            shape = tensor.shape
             # All open legs should have dimension 2
+            print(f"tensor shape: {shape}")
             for open_leg_index in node.open_legs:
                 self.assertEqual(2, shape[open_leg_index])
 
