19008f3e1e449be90751f7baad20cbfbfe047caa
Keefe Huang <keefe.huang@tum.de>
2023-06-29 11:14:55 +0200

    Correct __init__.py

--- pytreenet/__init__.py
+++ pytreenet/__init__.py
@@ -2,7 +2,7 @@
 
 from .canonical_form import *
 from .hamiltonian import *
-from .graph_node import *
+from .node import *
 from .leg_specification import *
 from .node_contraction import *
 from .time_evolution import *
@@ -10,7 +10,7 @@ from .tensor_util import *
 from .tree_contraction import *
 from .trotter import *
 from .ttn_exceptions import *
-from .node import *
+from .graph_node import *
 from .tree_structure import *
 from .ttn import *
 from .ttno import *
