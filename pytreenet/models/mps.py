fb053bb16de495299491702ebc65c44cb6c3e91f
julien1k <julien.kollmann@tum.de>
2023-07-06 14:09:02 +0200

    More flexible naming

--- pytreenet/models/mps.py
+++ pytreenet/models/mps.py
@@ -9,12 +9,12 @@ O = np.zeros((2, 2), dtype=complex)
 I = np.identity(2, dtype=complex)
 
 
-def mps_zero(num_sites, virtual_bond_dimension):
+def mps_zero(num_sites, virtual_bond_dimension, name_prefix="site"):
     zero = zero_state([virtual_bond_dimension, virtual_bond_dimension, 2])
     mps = QuantumTTState()
 
     for site in range(num_sites):
-        identifier = "site" + str(site)
+        identifier = name_prefix + str(site)
         if site == 0:
             # Open boundary conditions on first site
             node_mps = QuantumStateNode(zero[0], identifier=identifier)
@@ -25,7 +25,7 @@ def mps_zero(num_sites, virtual_bond_dimension):
                 node_mps = QuantumStateNode(zero[:,0,:], identifier=identifier)
             else:
                 node_mps = QuantumStateNode(zero, identifier=identifier)
-            parent_id = "site" + str(site - 1)
+            parent_id = name_prefix + str(site - 1)
             if site == 1:
                 # Due to boundary condition on first site
                 mps.add_child_to_parent(node_mps, 0, parent_id, 0)
@@ -35,7 +35,7 @@ def mps_zero(num_sites, virtual_bond_dimension):
     return mps
 
 
-def mps_heisenberg(num_sites, jx, jy, jz, h):
+def mps_heisenberg(num_sites, jx, jy, jz, h, name_prefix="site"):
     A = np.array([[I, O, O, O, O],
                   [X, O, O, O, O],
                   [Y, O, O, O, O],
@@ -50,10 +50,10 @@ def mps_heisenberg(num_sites, jx, jy, jz, h):
     ham = QuantumTTOperator()
 
     for site in range(num_sites):
-        identifier = "site" + str(site)
+        identifier = name_prefix + str(site)
         if site == 0:
             # Open boundary conditions on first site
-            node_ham = QuantumOperatorNode(A[2,:,:,:], identifier=identifier)
+            node_ham = QuantumOperatorNode(A[-1,:,:,:], identifier=identifier)
             ham.add_root(node_ham)
         elif 0 < site:
             if site == num_sites - 1:
@@ -61,7 +61,7 @@ def mps_heisenberg(num_sites, jx, jy, jz, h):
                 node_ham = QuantumOperatorNode(A[:,0,:,:], identifier=identifier)
             else:
                 node_ham = QuantumOperatorNode(A, identifier=identifier)
-            parent_id = "site" + str(site - 1)
+            parent_id = name_prefix + str(site - 1)
             if site == 1:
                 # Due to boundary condition on first site
                 ham.add_child_to_parent(node_ham, 0, parent_id, 0)
