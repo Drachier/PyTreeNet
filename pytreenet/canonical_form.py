916e93b7cd5a20a033620d414cb0725adfa7d28d
Keefe Huang <keefe.huang@tum.de>
2023-07-13 14:41:08 +0200

    Correct connectivity after QR splitting

--- pytreenet/canonical_form.py
+++ pytreenet/canonical_form.py
@@ -28,6 +28,7 @@ def canonical_form(ttn: TreeTensorNetwork, orthogonality_center_id: str):
 
     # Perform QR-decomposition on all TensorNodes but the orthogonality center
     for distance in reversed(range(1, maximum_distance+1)):
+        # Perform QR on nodes furthest away first.
         node_id_with_distance = [node_id for node_id in distance_dict.keys()
                                  if distance_dict[node_id] == distance]
 
@@ -35,6 +36,8 @@ def canonical_form(ttn: TreeTensorNetwork, orthogonality_center_id: str):
             node = ttn.nodes[node_id]
             minimum_distance_neighbour_id = _find_smallest_distance_neighbour(
                 node, distance_dict)
+
+            min_neighbour_node = ttn.nodes[minimum_distance_neighbour_id]
 
             q_legs = {"parent_leg": None,
                       "child_legs": copy(node.children),
@@ -52,6 +55,13 @@ def canonical_form(ttn: TreeTensorNetwork, orthogonality_center_id: str):
 
             ttn.split_node_qr(node_id, q_legs, r_legs,
                               q_identifier=node_id, r_identifier="R_tensor")
+
+            if node.is_child_of(minimum_distance_neighbour_id):
+                leg_index = min_neighbour_node.get_child_leg(node_id) - min_neighbour_node.nparents()
+                min_neighbour_node.children[leg_index] = "R_tensor"
+            else:
+                min_neighbour_node.remove_parent()
+                min_neighbour_node.add_parent("R_tensor")
 
             ttn.contract_nodes(minimum_distance_neighbour_id, "R_tensor",
                                new_identifier=minimum_distance_neighbour_id)
