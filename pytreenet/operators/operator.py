889555a4535f9b15d94f1f6af452f06db46a27a3
Drachier <r.milbradt@tum.de>
2023-07-17 13:22:50 +0200

    Implement functions to modify contained operator

--- pytreenet/operators/operator.py
+++ pytreenet/operators/operator.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 from typing import List, Union, Dict
+from copy import copy
 
 import numpy as np
 
@@ -15,10 +16,54 @@ class Operator:
 class NumericOperator(Operator):
     """
     An operator that holds the operator associated with it directly as an array.
+     If the associated array is not a matrix, we assume the following leg order:
+      `(input_node1, input_node2, ..., input_noden, output_node1, ... output_noden)`
     """
 
     def __init__(self, operator: np.ndarray, node_identifiers: List[str]):
+        assert operator.ndim % 2 == 0
         super().__init__(operator, node_identifiers)
+
+    def to_matrix(self) -> NumericOperator:
+        """
+        Returns a new NumericOperator whose operator is the current operator
+         but in matrix form, i.e. all input legs are combined and all output
+         legs are combined
+        """
+        half_dim = int(self.operator.ndim / 2)
+        new_dim = np.prod(self.operator.shape[0:half_dim])
+        matrix = self.operator.reshape((new_dim, new_dim))
+        return NumericOperator(matrix, copy(self.node_identifiers))
+
+    def to_tensor(self, dim : Union[int, None] = None,
+                  ttn : Union[TreeTensorNetwork, None]=None) -> NumericOperator:
+        """
+        Returns a NumericOperator whose operator is the current operator but in tensor form,
+         i.e. has one leg for each identifier in node_identifier. 
+
+        Args:
+            dim (Union[int, None], optional): If all nodes have the same open dimension
+             it can be given here. Defaults to None.
+            ttn (Union[TreeTensorNetwork, None], optional): If not all nodes habe the same open
+             dimension a TTN is needed as a reference. Defaults to None.
+
+        Raises:
+            ValueError: If all inputs are None
+
+        Returns:
+            NumericOperator: A NumericOperator whose operator is the current operator but in tensor
+             form, i.e. has one leg for each identifier in node_identifier. 
+        """
+        if dim is None and ttn is None:
+            errstr = "`dim` and `ttn` cannot both be `None`!"
+            raise ValueError(errstr)
+        if not dim is None:
+            shape = [dim for _ in range(0, len(self.node_identifiers))]
+        else:
+            shape = [ttn.nodes[node_id].open_dimension()
+                     for node_id in self.node_identifiers]
+        tensor = self.operator.reshape(shape * 2)
+        return NumericOperator(tensor, copy(self.node_identifiers))
 
     def is_unitary(self) -> bool:
         """
@@ -48,4 +93,4 @@ class SymbolicOperator(Operator):
             NumericOperator: The converted operator.
         """
         return NumericOperator(conversion_dict[self.operator],
-                               self.node_identifiers)       
+                               self.node_identifiers)
