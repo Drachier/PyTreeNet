50c04f35e5b8064366ebe266d9c44c9133b995bf
Drachier <r.milbradt@tum.de>
2023-08-01 15:04:21 +0200

    Remove unused 'node_contraction' module

--- pytreenet/__init__.py
+++ pytreenet/__init__.py
@@ -4,7 +4,6 @@ from .canonical_form import *
 from .hamiltonian import *
 from .node import *
 from .leg_specification import *
-from .node_contraction import *
 from .time_evolution import *
 from .tensor_util import *
 from .tree_contraction import *
