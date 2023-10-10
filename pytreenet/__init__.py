5ce3110ebe689c619bb30650205715209b7d33b1
Drachier <r.milbradt@tum.de>
2023-10-10 16:48:53 +0200

    Add special ttn to import

--- pytreenet/__init__.py
+++ pytreenet/__init__.py
@@ -9,6 +9,7 @@ from .tensor_util import *
 from .tree_contraction import *
 from .ttn_exceptions import *
 from .graph_node import *
+from .special_ttn import *
 from .tree_structure import *
 from .ttn import *
 from .ttno import *
