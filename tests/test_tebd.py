f52648bf5d3ccf9d4c7daaba48fbeadcb25d12ee
Drachier <r.milbradt@tum.de>
2023-10-26 14:24:24 +0200

    Improved Docstring

--- tests/test_tebd.py
+++ tests/test_tebd.py
@@ -1,31 +1,171 @@
 import unittest
 
+from copy import deepcopy
+
+import numpy as np
+from scipy.linalg import expm
+
 import pytreenet as ptn
 
-class TestTEBD(unittest.TestCase):
+class TestTEBDinit(unittest.TestCase):
 
     def setUp(self):
+        # Create mock objects for initialization
+        self.initial_state = ptn.random_small_ttns()
+        self.tensor_products = [ptn.random_tensor_product(self.initial_state,2),
+                                ptn.random_tensor_product(self.initial_state,2)]
+        self.trotter_splitting = ptn.TrotterSplitting(self.tensor_products)
+        self.time_step_size = 0.1
+        self.final_time = 1.0
+        self.operators = [ptn.random_tensor_product(self.initial_state, 1)]
+        self.max_bond_dim = 100
+        self.rel_tol = 1e-10
+        self.total_tol = 1e-15
 
+    def test_valid_init(self):
+        tebd = ptn.TEBD(self.initial_state, self.trotter_splitting,
+                        self.time_step_size, self.final_time, self.operators,
+                        self.max_bond_dim, self.rel_tol, self.total_tol)
+
+        # Test correct instatiation
+        self.assertEqual(tebd.initial_state, self.initial_state)
+        self.assertEqual(tebd.trotter_splitting, self.trotter_splitting)
+        self.assertEqual(tebd.time_step_size, self.time_step_size)
+        self.assertEqual(tebd.final_time, self.final_time)
+        self.assertEqual(tebd.operators, self.operators)
+        self.assertEqual(tebd.max_bond_dim, self.max_bond_dim)
+        self.assertEqual(tebd.rel_tol, self.rel_tol)
+        self.assertEqual(tebd.total_tol, self.total_tol)
+
+    def test_invalid_arguments(self):
+        self.assertRaises(ValueError, ptn.TEBD, self.initial_state, 
+                          self.trotter_splitting, self.time_step_size,
+                          self.final_time, self.operators, -1,
+                          self.rel_tol, self.total_tol)
+        self.assertRaises(ValueError, ptn.TEBD, self.initial_state, 
+                          self.trotter_splitting, self.time_step_size,
+                          self.final_time, self.operators, 0.4,
+                          self.rel_tol, self.total_tol)
+        self.assertRaises(ValueError, ptn.TEBD, self.initial_state, 
+                          self.trotter_splitting, self.time_step_size,
+                          self.final_time, self.operators, self.max_bond_dim,
+                          -1, self.total_tol)
+        self.assertRaises(ValueError, ptn.TEBD, self.initial_state, 
+                          self.trotter_splitting, self.time_step_size,
+                          self.final_time, self.operators, self.max_bond_dim,
+                          self.rel_tol, -1)
+
+class TestTEBDsmall(unittest.TestCase):
+    def setUp(self):
+        # We need a ttns to work with
+        self.ttns = ptn.random_small_ttns()
+
+        # Time parameters
+        self.time_step_size = 0.1
+        self.final_time = 1.0
+
+        # Trotter operators
+        operator_r = ptn.TensorProduct({"root": ptn.crandn((2,2))})
+        operator_r_c1 = ptn.TensorProduct({"root": ptn.crandn((2,2)),
+                                           "c1": ptn.crandn((3,3))})
+        operator_r_c2 = ptn.TensorProduct({"c2": ptn.crandn((4,4)), # Opposite order, to expected
+                                           "root": ptn.crandn((2,2))})
+        self.trotter_operators = [operator_r, operator_r_c1, operator_r_c2]
+        self.trotter = ptn.TrotterSplitting(self.trotter_operators)
+
+        # Operators to measure
+        num_ops = 3
+        self.meas_operators = [ptn.random_tensor_product(self.ttns, 2) for i in range(num_ops)]
+
+        # Deactivate Truncation
+        self.max_bond_dim = float("inf")
+        self.rel_tol = float("-inf")
+        self.total_tol = float("-inf")
+
+        # Initialise TEBD
+        self.tebd = ptn.TEBD(self.ttns, self.trotter, self.time_step_size,
+                             self.final_time, self.meas_operators,
+                             self.max_bond_dim, self.rel_tol, self.total_tol)
+
+        # Initialise TEBD No truncation
+        self.tebd_no_trunc = ptn.TEBD(self.ttns, self.trotter, self.time_step_size,
+                             self.final_time, self.meas_operators,
+                             float("inf"), float("-inf"), float("-inf"))
+
+    def test_apply_one_trotter_step_single_site(self):
+        reference_state = deepcopy(self.ttns)
+
+        factor = -1j * self.time_step_size
+        time_evol_matrix = expm(factor * self.trotter_operators[0]["root"])
+
+        # Check equality of both time-evolution operators
+        self.assertTrue(np.allclose(time_evol_matrix, self.tebd.exponents[0].operator))
+
+        self.tebd._apply_one_trotter_step_single_site(self.tebd.exponents[0])
+        # The initial state should be untouched by this
+        self.assertEqual(reference_state, self.tebd.initial_state)
+
+        reference_state = reference_state.completely_contract_tree()
+        reference_state = reference_state.root[1].reshape(24)
+        time_evol_matrix = np.kron(time_evol_matrix, np.eye(12))
+        reference_state = time_evol_matrix @ reference_state
+
+        found_state = self.tebd.state.completely_contract_tree(to_copy=True)
+        found_state = found_state.root[1].reshape(24)
+        self.assertTrue(np.allclose(reference_state, found_state))
+
+    def test_apply_one_trotter_step_two_site_order_as_in_nodes(self):
+        reference_state = deepcopy(self.ttns)
+        factor = -1j * self.time_step_size
+        tp = self.trotter_operators[1]
+        operator = np.kron(tp["root"], tp["c1"])
+        time_evol_matrix = expm(factor * operator)
+
+        # Check equality of both time-evolution operators
+        internal_op = self.tebd.exponents[1].operator.reshape(6,6)
+        self.assertTrue(np.allclose(time_evol_matrix, internal_op))
+
+        self.tebd_no_trunc._apply_one_trotter_step_two_site(self.tebd.exponents[1])
+        # The initial state should be untouched by this
+        self.assertEqual(reference_state, self.tebd_no_trunc.initial_state)
+
+        reference_state = reference_state.completely_contract_tree()
+        reference_state = reference_state.root[1].reshape(24)
+        time_evol_matrix = np.kron(time_evol_matrix, np.eye(4))
+        reference_state = time_evol_matrix @ reference_state
+
+        found_state = self.tebd_no_trunc.state.completely_contract_tree(to_copy=True)
+        found_state = found_state.root[1].reshape(24)
+        self.assertTrue(np.allclose(reference_state, found_state))
+
+    def test_apply_one_trotter_step_two_site_order_not_as_in_nodes(self):
+        reference_state = deepcopy(self.ttns)
+        factor = -1j * self.time_step_size
+        tp = self.trotter_operators[2]
+        operator = np.kron(tp["c2"], tp["root"])
+        time_evol_matrix = expm(factor * operator)
+
+        # Check equality of both time-evolution operators
+        internal_op = self.tebd.exponents[2].operator.reshape(8,8)
+        self.assertTrue(np.allclose(time_evol_matrix, internal_op))
+
+        self.tebd_no_trunc._apply_one_trotter_step_two_site(self.tebd.exponents[2])
+        # The initial state should be untouched by this
+        self.assertEqual(reference_state, self.tebd_no_trunc.initial_state)
+
+        reference_state = reference_state.completely_contract_tree()
+        reference_state = reference_state.root[1].transpose((2,0,1)).reshape(24)
+        time_evol_matrix = np.kron(time_evol_matrix, np.eye(3))
+        reference_state = time_evol_matrix @ reference_state
+
+        found_state = self.tebd_no_trunc.state.completely_contract_tree(to_copy=True)
+        found_state = found_state.root[1].transpose(1,0,2).reshape(24)
+        self.assertTrue(np.allclose(reference_state, found_state))
+
+class TestTEBD(unittest.TestCase):
+    def setUp(self):
         # We need a ttn to work on.
-        self.node1, self.tensor1 = ptn.random_tensor_node((2,2,2,2), identifier="site1")
-        self.node2, self.tensor2 = ptn.random_tensor_node((2,2,2), identifier="site2")
-        self.node3, self.tensor3 = ptn.random_tensor_node((2,2), identifier="site3")
-        self.node4, self.tensor4 = ptn.random_tensor_node((2,2,2), identifier="site4")
-        self.node5, self.tensor5 = ptn.random_tensor_node((2,2), identifier="site5")
-        self.node6, self.tensor6 = ptn.random_tensor_node((2,2,2,2), identifier="site6")
-        self.node7, self.tensor7 = ptn.random_tensor_node((2,2), identifier="site7")
-        self.node8, self.tensor8 = ptn.random_tensor_node((2,2), identifier="site8")
-
-        self.ttn = ptn.TreeTensorNetworkState()
-
-        self.ttn.add_root(self.node1, self.tensor1)
-        self.ttn.add_child_to_parent(self.node2, self.tensor2, 0, "site1", 0)
-        self.ttn.add_child_to_parent(self.node3, self.tensor3, 0, "site2", 1)
-        self.ttn.add_child_to_parent(self.node4, self.tensor4, 0, "site1", 1)
-        self.ttn.add_child_to_parent(self.node5, self.tensor5, 0, "site4", 1)
-        self.ttn.add_child_to_parent(self.node6, self.tensor6, 0, "site1", 2)
-        self.ttn.add_child_to_parent(self.node7, self.tensor7, 0, "site6", 1)
-        self.ttn.add_child_to_parent(self.node8, self.tensor8, 0, "site6", 2)
+        self.ttn = ptn.random_big_ttns("same_dimension")
 
         # We need a toy model and will use a simple Ising model
         X, _, Z = ptn.pauli_matrices()
@@ -74,46 +214,6 @@ class TestTEBD(unittest.TestCase):
 
         self.time_step_size = 0.1
         self.final_time = 1
-
-    def test_init(self):
-        tebd1 = ptn.TEBD(self.ttn, self.trotter_splitting_woswaps,
-                         self.time_step_size,
-                         self.final_time,
-                         self.operators)
-        self.assertEqual(7, len(tebd1.exponents))
-        tebd2 = ptn.TEBD(self.ttn, self.trotter_splitting_wswaps,
-                         self.time_step_size,
-                         self.final_time,
-                         self.operators)
-        self.assertEqual(15, len(tebd2.exponents))
-
-    def test_run_one_time_step(self):
-        # Setting up tebd
-        tebd = ptn.TEBD(self.ttn,
-                        self.trotter_splitting_wswaps,
-                        self.time_step_size,
-                        self.final_time,
-                        self.operators)
-        tebd.run_one_time_step()
-        self.assertEqual(8, len(tebd.state.nodes))
-        self.assertEqual(8, len(tebd.state.tensors))
-
-    def test_evaluate_operators(self):
-        # Setting up tebd
-        tebd = ptn.TEBD(self.ttn,
-                        self.trotter_splitting_wswaps,
-                        self.time_step_size,
-                        self.final_time,
-                        self.operators)
-        tebd.evaluate_operators()
-
-    def test_run(self):
-        tebd = ptn.TEBD(self.ttn,
-                        self.trotter_splitting_wswaps,
-                        self.time_step_size,
-                        self.final_time,
-                        self.operators)
-        tebd.run()
 
 if __name__ == "__main__":
     unittest.main()
