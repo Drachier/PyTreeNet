8088e9f2bad898d72b6529506a1fc3cd9c3ab909
Drachier <r.milbradt@tum.de>
2023-10-30 16:42:56 +0100

    Include fast_exp_action from tdvp_branch

--- pytreenet/util.py
+++ pytreenet/util.py
@@ -2,6 +2,10 @@
 Some useful tools
 """
 import numpy as np
+from scipy.linalg import expm as expm
+from scipy.sparse.linalg import expm_multiply, eigsh
+from scipy.sparse.linalg import expm as expm_sparse
+from scipy.sparse import csr_matrix
 
 from copy import deepcopy, copy
 from collections import Counter
@@ -41,3 +45,44 @@ def compare_lists_by_value(list1, list2):
     if Counter(list1) == Counter(list2):
         return True
     return False
+
+def fast_exp_action(exponent: np.ndarray,
+                    vector: np.ndarray,
+                    mode: str = "fastest") -> np.ndarray:
+    """
+    Result = exp( exponent) @ vector
+
+    Args:
+        exponent (np.ndarray): The exponent in matrix form.
+        vector (np.ndarray): The input vector in vector form.
+        mode (str, optional): The mode to use. Defaults to "fastest".
+
+    Raises:
+        NotImplementedError: If an unimplemented mode is used.
+
+    Returns:
+        np.ndarray: The result of the exponentiation and multiplication.
+    """
+    if mode == "fastest":
+        mode = "chebyshev"
+    if mode == "expm":
+        return expm(exponent) @ vector
+    if mode == "eigsh":
+        if exponent.shape[0] < 4:
+            return expm(exponent) @ vector
+        k = min(exponent.shape[0]-2, 8)
+        w, v, = eigsh(exponent, k=k)
+        return v @ np.diag(np.exp(w)) @ np.linalg.pinv(v) @ vector
+    if mode == "chebyshev":
+        return expm_multiply(exponent, vector,
+                             traceA=np.trace(exponent))
+    if mode == "sparse":
+        exponent = csr_matrix(exponent)
+        vector = csr_matrix(exponent).transpose()
+        exponent_ = expm_sparse(exponent)
+        result = exponent_.dot(vector)
+        return result.toarray()
+    if mode == "none":
+        return vector
+    errstr = mode + " is not a possible mode for exponent action!"
+    raise NotImplementedError(errstr)
