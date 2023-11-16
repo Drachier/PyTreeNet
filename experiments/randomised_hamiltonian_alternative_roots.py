1b06051e7df6e31ff948d1c05f16617fa0da0154
Drachier <r.milbradt@tum.de>
2023-11-16 11:56:27 +0100

    Move root 5 to first file

--- experiments/randomised_hamiltonian_alternative_roots.py
+++ experiments/randomised_hamiltonian_alternative_roots.py
@@ -6,33 +6,6 @@ from randomised_hamiltonians_to_TTNO import main
 
 import pytreenet as ptn
 
-def tree_root_at_5():
-    """
-    Generates the desired tree tensor network with root at site 5 used as a
-     reference to construct the Hamiltonian.
-    """
-    ttns = ptn.TreeTensorNetworkState()
-
-    # Physical legs come last
-    node1, tensor1 = ptn.random_tensor_node((1, 1, 2), identifier="site1")
-    node2, tensor2 = ptn.random_tensor_node((1, 1, 1, 2), identifier="site2")
-    node3, tensor3 = ptn.random_tensor_node((1, 2), identifier="site3")
-    node4, tensor4 = ptn.random_tensor_node((1, 2), identifier="site4")
-    node5, tensor5 = ptn.random_tensor_node((1, 1, 1, 2), identifier="site5")
-    node6, tensor6 = ptn.random_tensor_node((1, 2), identifier="site6")
-    node7, tensor7 = ptn.random_tensor_node((1, 1, 2), identifier="site7")
-    node8, tensor8 = ptn.random_tensor_node((1, 2), identifier="site8")
-
-    ttns.add_root(node5, tensor5)
-    ttns.add_child_to_parent(node1, tensor1, 0, "site5", 0)
-    ttns.add_child_to_parent(node2, tensor2, 0, "site1", 1)
-    ttns.add_child_to_parent(node3, tensor3, 0, "site2", 1)
-    ttns.add_child_to_parent(node4, tensor4, 0, "site2", 2)
-    ttns.add_child_to_parent(node6, tensor6, 0, "site5", 1)
-    ttns.add_child_to_parent(node7, tensor7, 0, "site5", 2)
-    ttns.add_child_to_parent(node8, tensor8, 0, "site7", 1)
-    return ttns
-
 def tree_root_at_6():
     """
     Generates the desired tree tensor network with root at site 6 used as a
@@ -64,9 +37,6 @@ if __name__ == "__main__":
     parser = ArgumentParser()
     parser.add_argument("filepath", type=str, nargs=1)
     filepath = vars(parser.parse_args())["filepath"][0]
-    filepath1 = filepath + "_root_at_5.hdf5"
-    print("Data will be saved in " + filepath1)
-    main(filepath1, tree_root_at_5())
     filepath2 = filepath + "_root_at_6.hdf5"
     print("Data will be saved in " + filepath2)
-    main(filepath2, tree_root_at_6())
+    main(filepath2, tree_root_at_6(),num_runs=10)
--- experiments/randomised_hamiltonians_to_TTNO.py
+++ experiments/randomised_hamiltonians_to_TTNO.py
@@ -10,7 +10,7 @@ from numpy.random import default_rng
 
 import pytreenet as ptn
 
-def build_reference_tree() -> ptn.TreeTensorNetworkState:
+def construct_tree_root_at_1() -> ptn.TreeTensorNetworkState:
     """
     Generates the desired tree tensor network used as a reference to construct
      the Hamiltonian.
@@ -37,6 +37,34 @@ def build_reference_tree() -> ptn.TreeTensorNetworkState:
     ttns.add_child_to_parent(node8, tensor8, 0, "site7", 1)
 
     return ttns
+
+def construct_tree_root_at_5():
+    """
+    Generates the desired tree tensor network with root at site 5 used as a
+     reference to construct the Hamiltonian.
+    """
+    ttns = ptn.TreeTensorNetworkState()
+
+    # Physical legs come last
+    node1, tensor1 = ptn.random_tensor_node((1, 1, 2), identifier="site1")
+    node2, tensor2 = ptn.random_tensor_node((1, 1, 1, 2), identifier="site2")
+    node3, tensor3 = ptn.random_tensor_node((1, 2), identifier="site3")
+    node4, tensor4 = ptn.random_tensor_node((1, 2), identifier="site4")
+    node5, tensor5 = ptn.random_tensor_node((1, 1, 1, 2), identifier="site5")
+    node6, tensor6 = ptn.random_tensor_node((1, 2), identifier="site6")
+    node7, tensor7 = ptn.random_tensor_node((1, 1, 2), identifier="site7")
+    node8, tensor8 = ptn.random_tensor_node((1, 2), identifier="site8")
+
+    ttns.add_root(node5, tensor5)
+    ttns.add_child_to_parent(node1, tensor1, 0, "site5", 0)
+    ttns.add_child_to_parent(node2, tensor2, 0, "site1", 1)
+    ttns.add_child_to_parent(node3, tensor3, 0, "site2", 1)
+    ttns.add_child_to_parent(node4, tensor4, 0, "site2", 2)
+    ttns.add_child_to_parent(node6, tensor6, 0, "site5", 1)
+    ttns.add_child_to_parent(node7, tensor7, 0, "site5", 2)
+    ttns.add_child_to_parent(node8, tensor8, 0, "site7", 1)
+    return ttns
+
 
 def save_metadata(file: Any, seed: int, max_num_terms: int, num_runs: int,
                   conversion_dict: Dict[str, np.ndarray],
@@ -114,7 +142,9 @@ def obtain_bond_dimensions(ttno: ptn.TTNO) -> np.ndarray:
             dimensions.append(node.parent_leg_dim())
     return np.asarray(dimensions)
 
-def main(filepath: str, ref_tree: ptn.TreeTensorNetworkState):
+def main(filename: str, ref_tree: ptn.TreeTensorNetworkState,
+         num_runs: int = 10000, min_num_terms: int=1,
+         max_num_terms: int = 30):
     # Prepare variables
     X, Y, Z = ptn.pauli_matrices()
     conversion_dict = {"X": X, "Y": Y, "Z": Z, "I2": np.eye(2, dtype="complex")}
@@ -123,13 +153,11 @@ def main(filepath: str, ref_tree: ptn.TreeTensorNetworkState):
     num_bonds = 7
     seed = 49892894
     rng = default_rng(seed=seed)
-    max_num_terms = 30
-    num_runs = 10000
 
-    with h5py.File(filepath, "w") as file:
+    with h5py.File(filename, "w") as file:
         save_metadata(file, seed, max_num_terms, num_runs, conversion_dict,
                       leg_dict)
-        for num_terms in tqdm(range(1, max_num_terms + 1)):
+        for num_terms in tqdm(range(min_num_terms, max_num_terms + 1)):
             grp = file.create_group(f"run_with_{num_terms}_terms")
             grp.attrs["num_terms"] = num_terms
             dset_svd = grp.create_dataset("svd_bond_dim",
@@ -153,12 +181,19 @@ def main(filepath: str, ref_tree: ptn.TreeTensorNetworkState):
                                                     mode=ptn.Decomposition.tSVD)
                     dset_ham[run, :] = obtain_bond_dimensions(ttno_ham)
                     dset_svd[run, :] = obtain_bond_dimensions(ttno_svd)
+                    if np.all(dset_ham[run, :] > dset_svd[run, :]):
+                        print(hamiltonian)
                     run += 1
 
 if __name__ == "__main__":
     parser = ArgumentParser()
     parser.add_argument("filepath", type=str, nargs=1)
     filepath = vars(parser.parse_args())["filepath"][0]
+    # For root at 1
+    filepath1 = filepath + "_root_at_1.hdf5"
     print("Data will be saved in " + filepath)
-    ref_tree = build_reference_tree()
-    main(filepath, ref_tree)
+    main(filepath1, construct_tree_root_at_1())
+    # For root at 5
+    filepath5 = filepath + "_root_at_5.hdf5"
+    print("Data will be saved in " + filepath5)
+    main(filepath5, construct_tree_root_at_5(), min_num_terms=30)
