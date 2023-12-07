22b3b94c412cf9085b033f1b6e4bf621da6084fc
Drachier <r.milbradt@tum.de>
2023-12-07 12:32:56 +0100

    test new graph node methods

--- tests/test_graph_node.py
+++ tests/test_graph_node.py
@@ -27,7 +27,6 @@ class TestGraphNode(unittest.TestCase):
         self.node = ptn.GraphNode(identifier="this")
 
     def test_add_parent(self):
-
         self.assertEqual(None, self.node.parent)
         self.node.add_parent(self.parent_id)
         self.assertEqual(self.parent_id, self.node.parent)
@@ -36,20 +35,20 @@ class TestGraphNode(unittest.TestCase):
         self.assertRaises(ValueError, self.node.add_parent, "some")
 
     def test_remove_parent(self):
-
         self.node.add_parent(self.parent_id)
         self.assertEqual(self.parent_id, self.node.parent)
         self.node.remove_parent()
         self.assertEqual(None, self.node.parent)
 
+    def test_remove_non_existing_parent(self):
+        self.node.remove_parent()
+        self.assertEqual(None,self.node.parent)
+
     def test_add_child(self):
-
         self.assertEqual([], self.node.children)
-
         # First child
         self.node.add_child(self.child1_id)
         self.assertEqual([self.child1_id], self.node.children)
-
         # Second child
         self.node.add_child(self.child2_id)
         self.assertEqual([self.child1_id, self.child2_id], self.node.children)
@@ -65,9 +64,10 @@ class TestGraphNode(unittest.TestCase):
         children.append("third_child")
         self.assertEqual(children, self.node.children)
 
-    def test_remove_child(self):
+    def test_remove_child_not_existing(self):
         self.assertRaises(ValueError, self.node.remove_child, "some")
 
+    def test_remove_child_existing(self):
         # If children exist
         self.node.add_children([self.child1_id, self.child2_id])
         self.node.remove_child(self.child1_id)
@@ -75,61 +75,128 @@ class TestGraphNode(unittest.TestCase):
         self.node.remove_child(self.child2_id)
         self.assertEqual([], self.node.children)
 
-    def test_child_index(self):
+    def test_child_index_not_existing(self):
         self.assertRaises(ValueError, self.node.child_index, "some")
 
+    def test_child_index_existing(self):
         # If children exist
         self.node.add_children([self.child1_id, self.child2_id])
         self.assertEqual(0, self.node.child_index(self.child1_id))
         self.assertEqual(1, self.node.child_index(self.child2_id))
 
-    def test_replace_child(self):
-        self.node.add_children([self.child1_id, self.child2_id])
+    def test_neighbour_index_not_existing(self):
+        self.assertRaises(ValueError, self.node.neighbour_index, "some")
 
+    def test_neighbour_index_children_only(self):
+        self.node.add_children([self.child1_id,self.child2_id])
+        self.assertEqual(0,self.node.neighbour_index(self.child1_id))
+        self.assertEqual(1,self.node.neighbour_index(self.child2_id))
+
+    def test_neighbour_index_parent_only(self):
+        self.node.add_parent(self.parent_id)
+        self.assertEqual(0,self.node.neighbour_index(self.parent_id))
+
+    def test_neighbour_index_parent_and_children(self):
+        self.node.add_parent(self.parent_id)
+        self.node.add_children([self.child1_id,self.child2_id])
+        self.assertEqual(0,self.node.neighbour_index(self.parent_id))
+        self.assertEqual(1,self.node.neighbour_index(self.child1_id))
+        self.assertEqual(2,self.node.neighbour_index(self.child2_id))
+
+    def test_replace_child_non_existent(self):
+        self.node.add_children([self.child1_id, self.child2_id])
+        self.assertRaises(ValueError,self.node.replace_child,
+                          "false","other")
+
+    def test_replace_child_same_identifier(self):
+        self.node.add_children([self.child1_id, self.child2_id])
         # Same identifier -> Nothing happens
         self.node.replace_child(self.child1_id, self.child1_id)
         self.assertEqual([self.child1_id, self.child2_id], self.node.children)
+
+    def test_replace_child(self):
+        self.node.add_children([self.child1_id, self.child2_id])
         self.node.replace_child(self.child1_id, "other")
         self.assertEqual(["other", self.child2_id], self.node.children)
         self.node.replace_child(self.child2_id, "other2")
         self.assertEqual(["other", "other2"], self.node.children)
 
-    def test_is_root(self):
+    def test_replace_neighbour_non_existing(self):
+        self.node.add_parent(self.parent_id)
+        self.node.add_children([self.child1_id,self.child2_id])
+        self.assertRaises(ValueError,self.node.replace_neighbour,
+                          "false","other")
+        
+    def test_replace_neighbour_parent(self):
+        self.node.add_parent(self.parent_id)
+        self.node.replace_neighbour(self.parent_id,"new")
+        self.assertEqual("new",self.node.parent)
+
+    def test_replace_neighbour_parent_with_children(self):
+        self.node.add_parent(self.parent_id)
+        self.node.add_children([self.child1_id,self.child2_id])
+        self.node.replace_neighbour(self.parent_id,"new")
+        self.assertEqual("new",self.node.parent)
+
+    def test_replace_neighbour_child(self):
+        self.node.add_children([self.child1_id,self.child2_id])
+        self.node.replace_neighbour(self.child1_id,"new")
+        self.assertEqual(["new",self.child2_id],self.node.children)
+        self.node.replace_neighbour(self.child2_id,"new2")
+        self.assertEqual(["new","new2"],self.node.children)
+
+    def test_replace_neigbour_children_with_parent(self):
+        self.node.add_parent(self.parent_id)
+        self.node.add_children([self.child1_id,self.child2_id])
+        self.node.replace_neighbour(self.child1_id,"new")
+        self.assertEqual(["new",self.child2_id],self.node.children)
+        self.node.replace_neighbour(self.child2_id,"new2")
+        self.assertEqual(["new","new2"],self.node.children)
+
+    def test_is_root_true(self):
         # It is
         self.assertTrue(self.node.is_root())
 
+    def test_is_root_false(self):
         # Isn't
         self.node.add_parent("1338")
         self.assertFalse(self.node.is_root())
 
-    def test_is_leaf(self):
+    def test_is_leaf_true(self):
         # It is
         self.assertTrue(self.node.is_leaf())
 
+    def test_is_leaf_false(self):
         # Isn't
         self.node.add_child("ABCDEFG")
         self.assertFalse(self.node.is_leaf())
 
-    def test_is_child_of(self):
+    def test_is_child_of_root(self):
         # Not if there is no parent
         self.assertFalse(self.node.is_child_of("hi"))
 
+    def test_is_child_of_false(self):
         # Wrong node
         self.node.add_parent("shark")
         self.assertFalse(self.node.is_child_of("hi"))
 
+    def test_is_child_of_true(self):
         # It is
+        self.node.add_parent("shark")
         self.assertTrue(self.node.is_child_of("shark"))
 
-    def test_is_parent_of(self):
+    def test_is_parent_of_leaf(self):
         # Not without children
         self.assertFalse(self.node.is_parent_of("fish"))
 
+    def test_is_parent_of_false(self):
         # Wrong child
         self.node.add_children([self.child1_id, self.child2_id])
         self.assertFalse(self.node.is_parent_of("More Fish!"))
 
+    def test_is_parent_of_true(self):
         # It is
+        self.node.add_children([self.child1_id, self.child2_id])
         self.assertTrue(self.node.is_parent_of(self.child1_id))
         self.assertTrue(self.node.is_parent_of(self.child2_id))
 
