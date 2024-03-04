10892bd3c7a5fee239cc6446aede47d03a2fe14b
Drachier <r.milbradt@tum.de>
2024-03-04 13:17:58 +0100

    Add non-negative check

--- pytreenet/ttn_exceptions.py
+++ pytreenet/ttn_exceptions.py
@@ -35,3 +35,22 @@ def positiviy_check(value: Union[int,float], name: Union[str,None] = None,
                 name = "value"
             errstr = f"The {name} has to be positive!"
         raise ValueError(errstr)
+
+def non_negativity_check(value: Union[int,float], name: Union[str,None] = None,
+                         errstr: Union[None,str] = None):
+    """
+    Check if a given value is non-negative, i.e >=0.
+
+    Args:
+        value (Union[int,float]): The value to check.
+        name (Union[str,None], optional): A name of the value that is checked,
+         will be inserted into a default error string. Defaults to None.
+        errstr (Union[None,str], optional): An individual error string that
+         overrides the default error string. Defaults to None.
+    """
+    if value < 0:
+        if errstr is None:
+            if name is None:
+                name = "value"
+            errstr = f"The {name} has to be non-negative!"
+        raise ValueError(errstr)
