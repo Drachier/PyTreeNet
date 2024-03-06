70932d46e22f9f4922a7c4dd445c7d5a42cd318a
Drachier <r.milbradt@tum.de>
2024-03-06 18:03:45 +0100

    Add quick check for canonical form

--- pytreenet/ttns/ttns.py
+++ pytreenet/ttns/ttns.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
-
+from typing import Union
 from copy import deepcopy
+
 import numpy as np
 
 from ..ttn import TreeTensorNetwork
@@ -9,7 +10,8 @@ from ..contractions.state_state_contraction import contract_two_ttns
 
 class TreeTensorNetworkState(TreeTensorNetwork):
     """
-    This class holds methods commonly used with tree tensor networks representing a state.
+    This class holds methods commonly used with tree tensor networks
+     representing a state.
     """
 
     def scalar_product(self) -> complex:
@@ -70,3 +72,29 @@ class TreeTensorNetworkState(TreeTensorNetwork):
         for node_id, single_site_operator in operator.items():
             ttn.absorb_into_open_legs(node_id, single_site_operator)
         return contract_two_ttns(ttn, conj_ttn)
+
+    def is_in_canonical_form(self, node_id: Union[None,str] = None) -> bool:
+        """
+        Returns whether the TTNS is in canonical form. If a node_id is specified,
+         it will check as if that node is the orthogonalisation center. If no
+         node_id is given, the current orthogonalisation center will be used.
+
+        Args:
+            node_id (Union[None,str], optional): The node to check. If None, the
+             current orthogonalisation center will be used. Defaults to None.
+        
+        Returns:
+            bool: Whether the TTNS is in canonical form.
+        """
+        if node_id is None:
+            node_id = self.orthogonality_center_id
+        if node_id is None:
+            return False
+        total_contraction = self.scalar_product()
+        local_tensor = self.tensors[node_id]
+        legs = range(local_tensor.ndim)
+        local_contraction = complex(np.tensordot(local_tensor, local_tensor.conj(),
+                                                 axes=(legs,legs)))
+        # If the TTNS is in canonical form, the contraction of the
+        # orthogonality center should be equal to the norm of the state.
+        return np.allclose(total_contraction, local_contraction)
