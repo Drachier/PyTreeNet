6ac427bc8a6f86d09bca1473cde37263cbba3594
Drachier <r.milbradt@tum.de>
2024-03-08 13:46:17 +0100

    Refactoring for easier readability

--- pytreenet/time_evolution/tdvp_algorithms/firstorderonesite.py
+++ pytreenet/time_evolution/tdvp_algorithms/firstorderonesite.py
@@ -9,29 +9,49 @@ class FirstOrderOneSiteTDVP(TDVPAlgorithm):
      This means we have first order Trotter splitting for the time evolution:
       exp(At+Bt) approx exp(At)*exp(Bt)
     """
+    def _assert_orth_center(self, node_id: str):
+        errstr = f"Node {node_id} is not the orthogonality center! It should be!"
+        assert self.state.orthogonality_center_id == node_id, errstr
 
-    def _update(self, node_id: str,
-                next_node_id: Union[str, None]):
-        assert self.state.orthogonality_center_id == node_id
+    def _assert_leaf_node(self, node_id: str):
+        errstr = f"Node {node_id} is not a leaf! It should be!"
+        assert self.state.nodes[node_id].is_leaf(), errstr
+
+    def _update_site_and_link(self, node_id: str, update_index: int):
+        assert update_index < len(self.orthogonalization_path) - 1
         self._update_site(node_id)
-        if next_node_id is not None:
-            self._update_link(node_id, next_node_id)
+        next_node_id = self.orthogonalization_path[update_index][0]
+        self._update_link(node_id, next_node_id)
 
-    def run_one_time_step(self):
-        for i, node_id in enumerate(self.update_path):
-            # Orthogonalize
-            if i>0:
-                self._move_orth_and_update_cache_for_path(self.orthogonalization_path[i-1])
-            # Select Next Node
-            if i+1 < len(self.update_path):
-                next_node_id = self.orthogonalization_path[i][0]
-            else:
-                next_node_id = None
-            # Update
-            self._update(node_id, next_node_id)
+    def _first_update(self, node_id: str):
+        self._assert_orth_center(node_id)
+        self._assert_leaf_node(node_id)
+        self._update_site_and_link(node_id, 0)
+
+    def _normal_update(self, node_id: str, update_index: int):
+        current_orth_path = self.orthogonalization_path[update_index-1]
+        self._move_orth_and_update_cache_for_path(current_orth_path)
+        self._update_site_and_link(node_id, update_index)
+
+    def _reset_for_next_time_step(self):
         # Orthogonalise for next time step
         self.state.move_orthogonalization_center(self.update_path[0],
                                                  mode = SplitMode.KEEP)
         # We have to recache all partial tree tensors
         self._init_partial_tree_cache()
-        
+
+    def _final_update(self, node_id: str):
+        current_orth_path = self.orthogonalization_path[-1]
+        self._move_orth_and_update_cache_for_path(current_orth_path)
+        self._assert_leaf_node(node_id) # The final site to be updated should be a leaf node
+        self._update_site(node_id)
+        self._reset_for_next_time_step()
+
+    def run_one_time_step(self):
+        for i, node_id in enumerate(self.update_path):
+            if i == 0:
+                self._first_update(node_id)
+            elif i == len(self.update_path)-1:
+                self._final_update(node_id)
+            else:
+                self._normal_update(node_id, i)
