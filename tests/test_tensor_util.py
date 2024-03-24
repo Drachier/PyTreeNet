bc1e11ad76b7f39d0f11970aa24bdf7e9f207a94
R. Milbradt <74064217+Drachier@users.noreply.github.com>
2024-03-24 18:12:52 +0100

    Test refactorised tensor util

--- tests/test_tensor_util.py
+++ tests/test_tensor_util.py
@@ -1,11 +1,13 @@
 import unittest
+from copy import deepcopy
+
 import numpy as np
 
 import pytreenet as ptn
 from pytreenet.tensor_util import _determine_tensor_shape
 
 
-class TestTreeTensorNetwork(unittest.TestCase):
+class TestTensorUtilSimple(unittest.TestCase):
     def setUp(self):
         self.tensor1 = ptn.crandn((2, 3, 4, 5))
         self.output_legs = (1, 3)
@@ -61,8 +63,9 @@ class TestTreeTensorNetwork(unittest.TestCase):
         self.assertTrue(np.allclose(identity, transfer_matrix))
 
     def test_tensor_svd(self):
-        u, s, vh = ptn.tensor_svd(
-            self.tensor1, self.output_legs, self.input_legs)
+        u, s, vh = ptn.tensor_svd(self.tensor1,
+                                  self.output_legs,
+                                  self.input_legs)
 
         self.assertEqual(u.shape[-1], len(s))
         self.assertEqual(vh.shape[0], len(s))
@@ -80,13 +83,10 @@ class TestTreeTensorNetwork(unittest.TestCase):
     def test_check_truncation_parameters(self):
         self.assertRaises(TypeError, ptn.check_truncation_parameters,
                           max_bond_dim=1.3,  rel_tol=0.01, total_tol=1e-15)
-
         self.assertRaises(ValueError, ptn.check_truncation_parameters,
                           max_bond_dim=-100,  rel_tol=0.01, total_tol=1e-15)
-
         self.assertRaises(ValueError, ptn.check_truncation_parameters,
                           max_bond_dim=100,  rel_tol=-2.0, total_tol=1e-15)
-
         self.assertRaises(ValueError, ptn.check_truncation_parameters,
                           max_bond_dim=100,  rel_tol=0.01, total_tol=-1)
 
@@ -209,6 +209,224 @@ class TestTensorQRDecomp(unittest.TestCase):
         self.assertTrue(np.allclose(ref_q, q))
         self.assertTrue(np.allclose(ref_r, r))
 
+class TestSingularValueDecompositions(unittest.TestCase):
+    def setUp(self):
+        self.tensor = ptn.crandn((2,3,4,5))
+        self.u_legs = (1,3)
+        self.v_legs = (0,2)
+        self.s_values = np.array([1.2,1,0.8,0.5,0.2,0.1,0.1,0.01])
+        self.sum_s = np.sum(self.s_values)
+
+    def test_svd_reduced(self):
+        """
+        Test the SVD of a tensor with REDUCED mode, i.e. the tensor legs
+         of the resulting U and V tensors pointing to S have the minimal
+         dimension possible.
+        """
+        reference_tensor = self.tensor.transpose((1,3,0,2))
+        reference_tensor = reference_tensor.reshape((15,8))
+        ref_u, ref_s, ref_v = np.linalg.svd(reference_tensor,
+                                            full_matrices=False)
+        ref_u = ref_u.reshape(3,5,8)
+        ref_v = ref_v.reshape(8,2,4)
+        u, s, vh = ptn.tensor_svd(self.tensor,self.u_legs,self.v_legs)
+        self.assertTrue(np.allclose(ref_u,u))
+        self.assertTrue(np.allclose(ref_s,s))
+        self.assertTrue(np.allclose(ref_v,vh))
+
+    def test_svd_full(self):
+        """
+        Test the SVD of a tensor in FULL mode, i.e. the tensor legs
+         of the resulting U and V tensors pointing to S have the size of all
+         other legs of the respective tensor taken together.
+        """
+        reference_tensor = self.tensor.transpose((1,3,0,2))
+        reference_tensor = reference_tensor.reshape((15,8))
+        ref_u, ref_s, ref_v = np.linalg.svd(reference_tensor,
+                                            full_matrices=True)
+        ref_u = ref_u.reshape(3,5,15)
+        ref_v = ref_v.reshape(8,2,4)
+        u, s, vh = ptn.tensor_svd(self.tensor,self.u_legs,self.v_legs,
+                                  mode=ptn.SplitMode.FULL)
+        self.assertTrue(np.allclose(ref_u,u))
+        self.assertTrue(np.allclose(ref_s,s))
+        self.assertTrue(np.allclose(ref_v,vh))
+
+    def test_svd_keep(self):
+        """
+        Test the SVD of a tensor in KEEP mode, i.e. the tensor legs
+         of the resulting U and V tensors pointing to S have the size of all
+         other legs of the respective tensor taken together.
+        """
+        reference_tensor = self.tensor.transpose((1,3,0,2))
+        reference_tensor = reference_tensor.reshape((15,8))
+        ref_u, ref_s, ref_v = np.linalg.svd(reference_tensor,
+                                            full_matrices=True)
+        ref_u = ref_u.reshape(3,5,15)
+        ref_v = ref_v.reshape(8,2,4)
+        u, s, vh = ptn.tensor_svd(self.tensor,self.u_legs,self.v_legs,
+                                  mode=ptn.SplitMode.KEEP)
+        self.assertTrue(np.allclose(ref_u,u))
+        self.assertTrue(np.allclose(ref_s,s))
+        self.assertTrue(np.allclose(ref_v,vh))
+
+    def test_renorm_singular_values(self):
+        """
+        Test the renormalisation of a random tensor of positive values which
+         represent singular values against a truncated vector.
+        """
+        s = np.random.rand(10)
+        s = np.array(list(reversed(np.sort(s))))
+        s_new = s[:5]
+        norm_old = np.sum(s)
+        norm_new = np.sum(s_new)
+        normed_s = s_new * (norm_old / norm_new)
+        found_s = ptn.renormalise_singular_values(s,s_new)
+        self.assertTrue(np.allclose(normed_s,found_s))
+
+    def test_renorm_singular_value_equal_size(self):
+        """
+        Test the renormalisation of a random tensor of positive values which
+         represent singular values against an equal vector.
+        """
+        s = np.random.rand(10)
+        found_s = ptn.renormalise_singular_values(deepcopy(s),deepcopy(s))
+        self.assertTrue(np.allclose(found_s,s))
+
+    def test_truncate_singular_values_no_truncation(self):
+        """
+        Tests the truncation of singular values, however no truncation should
+         happen given the parameters.
+        """
+        found_s, truncated_s = ptn.truncate_singular_values(deepcopy(self.s_values),
+                                               10,0,0)
+        self.assertTrue(np.allclose(self.s_values,found_s))
+        self.assertTrue(np.allclose(np.array([]),truncated_s))
+
+    def test_truncate_singular_values_max_bond_dim(self):
+        """
+        Tests the truncation of singular values caused by the maximum bond
+         dimension given.
+        """
+        found_s, truncated_s = ptn.truncate_singular_values(deepcopy(self.s_values),
+                                                            6,0,0)
+        correct_s = self.s_values[:6] * self.sum_s / np.sum(self.s_values[:6])
+        self.assertTrue(np.allclose(correct_s, found_s))
+        self.assertTrue(np.allclose(np.array([0.1,0.01]),truncated_s))
+
+    def test_truncate_singular_values_abs_tol(self):
+        """
+        Tests the truncation of singular values caused by the absolute
+         tolerance given.
+        """
+        abs_tol = 0.15
+        found_s, truncated_s = ptn.truncate_singular_values(deepcopy(self.s_values),
+                                                            10,0,abs_tol)
+        correct_s = self.s_values[:5] * self.sum_s / np.sum(self.s_values[:5])
+        self.assertTrue(np.allclose(correct_s,found_s))
+        correct_trunc = self.s_values[5:]
+        self.assertTrue(np.allclose(correct_trunc,truncated_s))
+
+    def test_truncate_singular_values_rel_tol(self):
+        """
+        Tests the truncation of singular values caused by the relative
+         tolerance given.
+        """
+        rel_tol = 0.5
+        found_s, truncated_s = ptn.truncate_singular_values(deepcopy(self.s_values),
+                                                            10,rel_tol,0)
+        correct_s = self.s_values[:3]* self.sum_s / np.sum(self.s_values[:3])
+        self.assertTrue(np.allclose(correct_s,found_s))
+        correct_trunc = self.s_values[3:]
+        self.assertTrue(np.allclose(correct_trunc,truncated_s))
+
+    def test_truncate_singular_values_all_truncated(self):
+        """
+        Tests the truncation of singular values if all singular values were
+            truncated.
+        """
+        found_s, truncated_s = ptn.truncate_singular_values(deepcopy(self.s_values),
+                                                            10,0,3)
+        print("TEST SPEAKING: This warning is intended to be shown!")
+        correct_s = np.array([self.s_values[0]]) * self.sum_s / self.s_values[0]
+        self.assertTrue(np.allclose(correct_s,found_s))
+        self.assertTrue(np.allclose(self.s_values[1:],truncated_s))
+        self.assertWarns(UserWarning,ptn.truncate_singular_values,
+                         deepcopy(self.s_values),
+                         10,0,3)
+
+    def test_truncated_tensor_svd(self):
+        """
+        Tests the truncated SVD of a random tensor.
+        """
+        u, s, vh = ptn.truncated_tensor_svd(self.tensor,self.u_legs,self.v_legs,
+                                            6,0,0)
+        u_ref, s_ref, vh_ref = ptn.tensor_svd(self.tensor,self.u_legs,self.v_legs,
+                                              mode=ptn.SplitMode.FULL)
+        u_ref = u_ref[:,:,:6]
+        s_ref = s_ref[:6] * np.sum(s_ref) / np.sum(s_ref[:6])
+        vh_ref = vh_ref[:6,:,:]
+        self.assertTrue(np.allclose(u_ref,u))
+        self.assertTrue(np.allclose(s_ref,s))
+        self.assertTrue(np.allclose(vh_ref,vh))
+
+    def test_contr_truncdated_tensor_svd_v_contr(self):
+        """
+        Tests the contracted truncated tensor svd, for which the singular
+         values are contracted into the V-tensor.
+        """
+        u, vh = ptn.contr_truncated_svd_splitting(self.tensor,
+                                                  self.u_legs,self.v_legs,
+                                                  max_bond_dim=6,
+                                                  rel_tol=0,
+                                                  total_tol=0)
+        u_ref, s_ref, vh_ref = ptn.tensor_svd(self.tensor,self.u_legs,self.v_legs)
+        u_ref = u_ref[:,:,:6]
+        s_ref = s_ref[:6] * np.sum(s_ref) / np.sum(s_ref[:6])
+        vh_ref = vh_ref[:6,:,:]
+        vh_ref = np.tensordot(np.diag(s_ref),vh_ref,axes=(1,0))
+        self.assertTrue(np.allclose(u_ref,u))
+        self.assertTrue(np.allclose(vh_ref,vh))
+
+    def test_contr_truncdated_tensor_svd_u_contr(self):
+        """
+        Tests the contracted truncated tensor svd, for which the singular
+         values are contracted into the U-tensor.
+        """
+        u, vh = ptn.contr_truncated_svd_splitting(self.tensor,
+                                                  self.u_legs,self.v_legs,
+                                                  contr_mode=ptn.ContractionMode.UCONTR,
+                                                  max_bond_dim=6,
+                                                  rel_tol=0,
+                                                  total_tol=0)
+        u_ref, s_ref, vh_ref = ptn.tensor_svd(self.tensor,self.u_legs,self.v_legs)
+        u_ref = u_ref[:,:,:6]
+        s_ref = s_ref[:6] * np.sum(s_ref) / np.sum(s_ref[:6])
+        vh_ref = vh_ref[:6,:,:]
+        u_ref = np.tensordot(u_ref,np.diag(s_ref),axes=(-1,0))
+        self.assertTrue(np.allclose(u_ref,u))
+        self.assertTrue(np.allclose(vh_ref,vh))
+
+    def test_contr_truncdated_tensor_svd_equal_contr(self):
+        """
+        Tests the contracted truncated tensor svd, for which the singular
+         values are distributed equally between U and V-tensors.
+        """
+        u, vh = ptn.contr_truncated_svd_splitting(self.tensor,
+                                                  self.u_legs,self.v_legs,
+                                                  contr_mode=ptn.ContractionMode.EQUAL,
+                                                  max_bond_dim=6,
+                                                  rel_tol=0,
+                                                  total_tol=0)
+        u_ref, s_ref, vh_ref = ptn.tensor_svd(self.tensor,self.u_legs,self.v_legs)
+        u_ref = u_ref[:,:,:6]
+        s_ref = s_ref[:6] * np.sum(s_ref) / np.sum(s_ref[:6])
+        vh_ref = vh_ref[:6,:,:]
+        s_ref = np.sqrt(s_ref)
+        u_ref = np.tensordot(u_ref,np.diag(s_ref),axes=(-1,0))
+        vh_ref = np.tensordot(np.diag(s_ref),vh_ref,axes=(1,0))
+        self.assertTrue(np.allclose(u_ref,u))
+        self.assertTrue(np.allclose(vh_ref,vh))
 
 if __name__ == "__main__":
     unittest.main()
