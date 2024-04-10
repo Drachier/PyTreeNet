4295544989291cc4bae5cd7b1b48753b6d3eb40d
R. Milbradt <74064217+Drachier@users.noreply.github.com>
2024-04-10 10:20:02 +0200

    Create module and class Docstring

--- pytreenet/core/leg_specification.py
+++ pytreenet/core/leg_specification.py
@@ -1,5 +1,48 @@
 """
-Intendend for internal use to specify legs before splitting tensors.
+This module is used to unambigously define tensor legs.
+
+It contains the LegSpecification class. This class allows the definition of
+legs with regards to legs towards neighbours and open legs. It is commonly
+used to define the which legs belong to which resulting node yielded by the
+splitting of a tensor. It allows the use of node identifiers rather than leg
+indices, avoiding potential complications.
+
+Example:
+    Assume we want to perform the following split
+
+                |open1  |open 0                             |open1          |open0
+             ___|_______|___                             ___|___         ___|___
+     child1 |               |   parent           child1 |       |  new  |       |   parent
+        ----|       C       |-----          -->     ----|   A   |-------|   B   |----
+            |_______________|                           |_______|       |_______|
+                |       |                                   |               |
+          child2|       |child3                       child2|         child3|
+    
+    the corresponding code would be:
+    ```python
+        # Creating the mother node
+        ctensor = crandn((2,2,2,2,2,2))
+        C = Node(identifier="C",tensor=ctensor)
+        C.open_leg_to_parent("parent",0)
+        children = {"child1":1,"child2":2,"child3":3}
+        C.open_leg_to_children()
+
+        # Defining the neighbouring legs
+        parent_legA = None
+        parent_legB = "parent"
+        children_legsA = ["child1","child2"]
+        children_legsB = ["child3"]
+
+        # Defining the open legs
+        open_legsA = [5]
+        open_legsB = [4]
+
+        # The LegSpecification
+        legsA = LegSpecification(parent_legA, children_legsA, open_legsA
+                                 node=C)
+        legsB = LegSpecification(parent_legB, children_legsB, open_legsB
+                                 node=C)
+    ```
 """
 from __future__ import annotations
 from typing import Union, List
@@ -8,8 +51,31 @@ from .node import Node
 
 class LegSpecification():
     """
-    Contains useful function and all required data to specify which legs
-     are part of which tensor in case of a tensor splitting.
+    Contains data to describe a split-off node's legs fully.
+
+    In case a splitting of node occurs, a LegSpecification contains all the
+    data required to define one of the new nodes. Can also be used in other
+    cases where a convenient storage of node legs is required.
+
+    Attributes:
+        parent_leg (Union[str,None]): The identifier of a potential parent.
+            If it is None, the node is either a root or the parent node is
+            created after the LegSpecification and the identifier is not yet
+            known.
+        children_legs (List[str]): The identifiers of the children that should
+            be associated with this node.
+        open_legs (List[int]): A list of open leg indices. The indices are
+            usually the indices of the tensor before a split or similar
+            operation. They are usually not the indices of the open legs of
+            newly created nodes.
+        node (Union[Node,None], optional): A node that is required to
+            translate the   stored identifiers into indices. Can be None, as
+            the node needing it might be created at a later point. Defaults to
+            None.
+        is_root (bool): Determines, if the node associated before a potential
+            split is a root and the node this specification is for should
+            suceed it in that. Not having a parent leg is insufficient, as this
+            might be created at a later date.
     """
 
     def __init__(self, parent_leg: Union[str, None], child_legs: List[str],
