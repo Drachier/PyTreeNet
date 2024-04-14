36c5362fcff9ed4a6fd1effca60bd10c2ae1498f
R. Milbradt <74064217+Drachier@users.noreply.github.com>
2024-04-14 17:51:23 +0200

    Update Docstrings of Canonical Form

--- pytreenet/core/canonical_form.py
+++ pytreenet/core/canonical_form.py
@@ -1,3 +1,11 @@
+"""
+This module is concerned with the canonical form of a TreeTensorNetwork.
+
+The canonical form of a TreeTensorNetwork is a specific choice of the gauge
+freedom causing all tensors apart from the orthogonality center to be a
+equivalent to an isometry. This is achieved by performing a series of QR
+decompositions on the tensors of the network.
+"""
 from __future__ import annotations
 from typing import Tuple
 from uuid import uuid1
@@ -8,31 +16,23 @@ from .leg_specification import LegSpecification
 from .node import Node
 from ..util.tensor_util import SplitMode
 
-def canonical_form(ttn: TreeTensorNetwork, orthogonality_center_id: str,
+def canonical_form(ttn: TreeTensorNetwork,
+                   orthogonality_center_id: str,
                    mode: SplitMode = SplitMode.REDUCED):
     """
-    Modifies the TreeTensorNetwork (ttn) into canonical form with the center of orthogonality at
-    node with node_id `orthogonality_center_id`.
+    Modifies a TreeTensorNetwork into canonical form.
 
-    Parameters
-    ----------
-    tree_tensor_network : TreeTensorNetwork
-        The TTN for which to find the canonical form
-    orthogonality_center_id : str
-        The id of the tensor node which is the orthogonality center for
-        the canonical form
-    mode: The mode to be used for the QR decomposition. For details refer to
-     `tensor_util.tensor_qr_decomposition`.
-
-    Returns
-    -------
-    None.
-
+    Args:
+        ttn (TreeTensorNetwork): The TTN for which to be transformed into
+            canonical form.
+        orthogonality_center_id (str): The identifier of the tensor node which
+            is the orthogonality center for the canonical form.
+        mode: The mode to be used for the QR decomposition. For details refe
+            to `tensor_util.tensor_qr_decomposition`.
     """
     distance_dict = ttn.distance_to_node(
         orthogonality_center_id)
     maximum_distance = max(distance_dict.values())
-
     # Perform QR-decomposition on all TensorNodes but the orthogonality center
     for distance in reversed(range(1, maximum_distance+1)):
         # Perform QR on nodes furthest away first.
@@ -48,24 +48,19 @@ def canonical_form(ttn: TreeTensorNetwork, orthogonality_center_id: str,
                                              mode=mode)
     ttn.orthogonality_center_id = orthogonality_center_id
 
-def _find_smallest_distance_neighbour(node: Node, distance_dict: dict[str, int]) -> str:
+def _find_smallest_distance_neighbour(node: Node,
+                                      distance_dict: dict[str, int]) -> str:
     """
-    Finds identifier of the neighbour of node with the minimal distance in
-    distance dict, i.e. minimum distance to the orthogonality center.
+    Finds the neighbour of a node with the smallest distance to the center node.
+    
+    Args:
+        node (Node): The node for which to search the neighbours.
+        distance_dict (dict[str,int]): A dictionary with the distance of every
+            node to the orthogonality center.
 
-    Parameters
-    ----------
-    node : Node
-        Node for which to search.
-    distance_dict : dict
-        Dictionary with the distance of every node to the orthogonality center.
-
-    Returns
-    -------
-    minimum_distance_neighbour_id : str
-        Identifier of the neighbour of node with minimum distance to in
-        distance_dict.
-
+    Returns:
+        str: The identifier of the neighbour of the node with the smallest
+            distance to the orthogonality center.
     """
     neighbour_ids = node.neighbouring_nodes()
     neighbour_distance_dict = {neighbour_id: distance_dict[neighbour_id]
@@ -92,11 +87,11 @@ def split_qr_contract_r_to_neighbour(ttn: TreeTensorNetwork,
 
     Args:
         ttn (TreeTensorNetwork): The tree tensor network in which to perform
-         this action.
+            this action.
         node_id (str): The identifier of the node to be split.
         neighbour_id (str): The identifier of the neigbour to which to split.
         mode: The mode to be used for the QR decomposition. For details refer to
-        `tensor_util.tensor_qr_decomposition`.
+            `tensor_util.tensor_qr_decomposition`.
     """
     node = ttn.nodes[node_id]
     q_legs, r_legs = _build_qr_leg_specs(node, neighbour_id)
@@ -121,9 +116,9 @@ def _build_qr_leg_specs(node: Node,
 
     Returns:
         Tuple[LegSpecification,LegSpecification]: 
-         The leg specifications for the legs of the Q-tensor, i.e. what
-          remains as the node, and the R-tensor, i.e. what will be absorbed
-          into the node defined by `min_neighbour_id`.
+            The leg specifications for the legs of the Q-tensor, i.e. what
+            remains as the node, and the R-tensor, i.e. what will be absorbed
+            into the node defined by `min_neighbour_id`.
     """
     q_legs = LegSpecification(None, copy(node.children), node.open_legs)
     if node.is_child_of(min_neighbour_id):
