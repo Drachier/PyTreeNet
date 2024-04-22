2994a27a531b53667bfc29acaadafe8ae3ff8373
R. Milbradt <74064217+Drachier@users.noreply.github.com>
2024-04-22 17:16:04 +0200

    Update Docstring of TEBD

--- pytreenet/time_evolution/tebd.py
+++ pytreenet/time_evolution/tebd.py
@@ -1,3 +1,6 @@
+"""
+Module implementing the TEBD time-evolution for TTNS.
+"""
 from __future__ import annotations
 from typing import List, Union
 
@@ -10,46 +13,51 @@ from ..util.tensor_splitting import SVDParameters
 
 class TEBD(TTNTimeEvolution):
     """
-    Runs the TEBD algorithm on a TTN
+    Runs the TEBD algorithm on a TTNS.
+
+    The TEBD algorithm uses a trotterised version of a Hamiltonian time
+    evolution. The different Trotter operators are contracted into the system
+    one by one. A truncation happens, if it is desired.
+
+    Attributes:
+        trotter_splitting (TrotterSplitting): A splitting of a Hamiltonian into
+            different few site operators. Currently only one and two site
+            operators are considered.
     """
 
     def __init__(self, initial_state: TreeTensorNetworkState,
                  trotter_splitting: TrotterSplitting, time_step_size: float,
                  final_time: float, operators: Union[List[TensorProduct], TensorProduct],
-                 svd_parameters: SVDParameters = SVDParameters(),
+                 svd_parameters: Union[SVDParameters,None] = None,
                  config: Union[TTNTimeEvolutionConfig,None] = None):
         """
-        A class that can be used to time evolve an initial state in the form
-         a tree tensor network state via the TEBD algorithm.
-
-        If no truncation is desired set max_bond_dim = inf, rel_tol = -inf,
-         and total_tol = -inf.
+        Initiates a TEBD object.
 
         Args:
             initial_state (TreeTensorNetworkState)): The initial state of our
-             time-evolution
+                time-evolution as a TTNS
             trotter_splitting (TrotterSplitting): The Trotter splitting to be
-             used for time-evolution.
+                used for time-evolution.
             time_step_size (float): The time step size to be used.
             final_time (float): The final time until which to run.
-            operators (Union[List[TensorProduct], TensorProduct]): Operators in the form of single site
-             tensor product for which expectation values should be determined.
-            max_bond_dim (int, optional): The maximum bond dimension allowed between
-            nodes. Defaults to 100.
-            rel_tol (float, optional): singular values s for which
-             ( s / largest singular value) < rel_tol are truncated. Defaults to 1e-10.
-            total_tol (float, optional): singular values s for which s < total_tol
-             are truncated. Defaults to 1e-15.
-
-        Raises:
-            ValueError: If the trotter splitting and TTNS are not compatible.
+            operators (Union[List[TensorProduct], TensorProduct]): Operators in
+                the form of single site tensor product for which expectation
+                values should be determined.
+            svd_parameters (Union[SVDParameters,None]): The parameters for the
+                SVD to be used in the truncations. The maximum bond dimension,
+                relative tolerance, and absolute tolerance can be given. It can
+                also be decided, if the singular values should be renormalised
+                after truncation. If None is given, the values default to zero.
         """
         super().__init__(initial_state,
                          time_step_size, final_time,
                          operators,
                          None)
         self._trotter_splitting = trotter_splitting
-        self.svd_parameters = svd_parameters
+        if svd_parameters is None:
+            self.svd_parameters = SVDParameters()
+        else:
+            self.svd_parameters = svd_parameters
         self._exponents = self._trotter_splitting.exponentiate_splitting(self._time_step_size,
                                                                          self.state)
 
@@ -71,25 +79,26 @@ class TEBD(TTNTimeEvolution):
         """
         Applies a single-site exponential operator of the Trotter splitting.
 
-        exp @ |state>
+                exp @ |state>
 
         Args:
             single_site_exponent (NumericOperator): An operator representing a
-             single-site unitary operator.
+                single-site unitary operator.
         """
         operator = single_site_exponent.operator.T
         identifier = single_site_exponent.node_identifiers[0]
         self.state.absorb_into_open_legs(identifier, operator)
 
-    def _apply_one_trotter_step_two_site(self, two_site_exponent: NumericOperator):
+    def _apply_one_trotter_step_two_site(self,
+                                         two_site_exponent: NumericOperator):
         """
         Applies the two-site exponential operator of the Trotter splitting.
-            exp @ |state>
+                exp @ |state>
 
         Args:
             two_site_exponent (NumericOperator): The exponent which should be
-             applied. Contains the numeric value and the identifier of the 
-             sites on which application should happen.
+                applied. Contains the numeric value and the identifier of the 
+                sites on which application should happen.
         """
         operator = two_site_exponent.operator.transpose([2,3,0,1])
         identifiers = two_site_exponent.node_identifiers
@@ -107,13 +116,13 @@ class TEBD(TTNTimeEvolution):
     def _apply_one_trotter_step(self, unitary: NumericOperator):
         """
         Applies the exponential operator of the Trotter splitting to the
-         current state.
-            exp(op) @ |state>
+            current state.
+                exp(op) @ |state>
 
         Args:
             unitary (NumericOperator): The exponent which should be
-             applied. Contains the numeric value and the identifier of the 
-             sites on which application should happen.
+                applied. Contains the numeric value and the identifier of the 
+                sites on which application should happen.
 
         Raises:
             NotImplementedError: If the operator acts on more than two sites.
@@ -127,14 +136,14 @@ class TEBD(TTNTimeEvolution):
         elif num_of_sites_acted_upon == 2:
             self._apply_one_trotter_step_two_site(unitary)
         else:
-            raise NotImplementedError(
-                "More than two-site interactions are not implemented.")
+            errstr = "More than two-site interactions are not implemented."
+            raise NotImplementedError(errstr)
 
-    def run_one_time_step(self):
+    def run_one_time_step(self, **kwargs):
         """
-        Running one time_step on the TNS according to the exponentials. The
-        order in which the trotter splitting is run, is the order in which the
-        time-evolution operators are saved in `self.exponents`.
+        Runs one TEBD time step.
+
+        This means every Trotter operator is applied once to the TTNS.
         """
         for unitary in self.exponents:
             self._apply_one_trotter_step(unitary)
