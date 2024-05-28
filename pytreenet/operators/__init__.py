d7570a4d5b9f0296e8cf74de19073b70710111df
R. Milbradt <74064217+Drachier@users.noreply.github.com>
2024-05-28 10:41:23 +0200

    Submodule Docstring

--- pytreenet/operators/__init__.py
+++ pytreenet/operators/__init__.py
@@ -1,3 +1,8 @@
+"""
+This submodule provides all kinds of operators.
+
+They do not have to have a tree form.
+"""
 from .common_operators import *
 from .hamiltonian import *
 from .operator import *
