fc452d2cbd4c6c80d48d7bf94c925cbec4883a38
R. Milbradt <74064217+Drachier@users.noreply.github.com>
2024-05-28 10:48:09 +0200

    Update TDVP __init__.py

--- pytreenet/time_evolution/tdvp_algorithms/__init__.py
+++ pytreenet/time_evolution/tdvp_algorithms/__init__.py
@@ -1,5 +1,11 @@
-from .onesitetdvp import *
+"""
+This submodule provides all potentially abstract TDVP algorithms.
+
+It should mostly be used to create new variations of the TDVP algorithms.
+"""
 from .firstorderonesite import *
+from .onesitetdvp import *
 from .secondorderonesite import *
-from .twositetdvp import *
 from .secondordertwosite import *
+from .tdvp_algorithm import *
+from .twositetdvp import *
