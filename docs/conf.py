3a82c500132cb28336d435b33b71de396924b55b
Drachier <r.milbradt@tum.de>
2024-06-20 17:03:36 +0200

    Fix napoleoon import

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,7 @@ release = '1.0.0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['sphinxcontrib.napoleon',
+extensions = ['sphinx.ext.napoleon',
               'sphinx.ext.autosummary',
               'sphinx.ext.autodoc',
               'sphinx.ext.mathjax',]
