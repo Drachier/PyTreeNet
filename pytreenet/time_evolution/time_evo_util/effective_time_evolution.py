428858dca1bae7e33088a1d41debc96d63b8a42d
Drachier <r.milbradt@tum.de>
2025-01-16 16:49:38 +0100

    Allow forward and backward effective time evolution

--- pytreenet/time_evolution/time_evo_util/effective_time_evolution.py
+++ pytreenet/time_evolution/time_evo_util/effective_time_evolution.py
@@ -12,13 +12,16 @@ from pytreenet.ttns import TreeTensorNetworkState
 from pytreenet.ttno import TreeTensorNetworkOperator
 from pytreenet.contractions.sandwich_caching import SandwichCache
 from pytreenet.contractions.effective_hamiltonians import get_effective_single_site_hamiltonian
-from pytreenet.time_evolution.time_evolution import time_evolve
+from pytreenet.time_evolution.time_evolution import time_evolve, TimeEvoMode
 
 def single_site_time_evolution(node_id: str,
                                state: TreeTensorNetworkState,
                                hamiltonian: TreeTensorNetworkOperator,
                                time_step_size: float,
-                               tensor_cache: SandwichCache) -> ndarray:
+                               tensor_cache: SandwichCache,
+                               forward: bool = True,
+                               mode: TimeEvoMode = TimeEvoMode.FASTEST
+                               ) -> ndarray:
     """
     Perform the time evolution for a single site.
 
@@ -32,6 +35,10 @@ def single_site_time_evolution(node_id: str,
         hamiltonian (TreeTensorNetworkOperator): The Hamiltonian of the system.
         time_step_size (float): The time step size.
         tensor_cache (SandwichCache): The cache for the neighbour blocks.
+        forward (bool): Whether to time evolve forward or backward. Defaults to
+            True.
+        mode (TimeEvoMode): The mode of the time evolution. Defaults to
+            TimeEvoMode.FASTEST
 
     Returns:
         ndarray: The updated tensor.
@@ -43,5 +50,7 @@ def single_site_time_evolution(node_id: str,
                                                     tensor_cache)
     updated_tensor = time_evolve(state.tensors[node_id],
                                  ham_eff,
-                                 time_step_size)
+                                 time_step_size,
+                                 forward=forward,
+                                 mode=mode)
     return updated_tensor
