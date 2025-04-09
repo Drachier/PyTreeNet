9e5d276cc199222bb58bca722aebaa29792b7c7f
R. Milbradt <74064217+Drachier@users.noreply.github.com>
2025-04-08 22:21:45 -0700

    Fix QNTTN

--- pytreenet/core/quantum_numbers/qn_ttn.py
+++ pytreenet/core/quantum_numbers/qn_ttn.py
@@ -21,6 +21,18 @@ class QNTTN(TreeTensorNetwork):
         super().__init__()
         self._nodes: dict[int, QNNode]
 
+    def ensure_shape_matching(self,
+                              new_tensor, tensor_leg,
+                              old_node, old_leg,
+                              new_node_id = None):
+        # Since QN nodes are always attached using a tuple of tensor and 
+        # quantum numbers, we need to hand over only the tensor to the
+        # super class.
+        return super().ensure_shape_matching(new_tensor[0], tensor_leg,
+                                             old_node, old_leg,
+                                             new_node_id)
+        # TODO: Implement check for compatible QN
+
     def find_open_qn(self,
                      node_ids: list[str]
                      ) -> list[ndarray]:
@@ -41,6 +53,8 @@ class QNTTN(TreeTensorNetwork):
             for neighbour_id in node.neighbouring_nodes():
                 if neighbour_id not in node_ids:
                     open_qns.append(node.get_neighbour_qn(neighbour_id))
+            for leg_ind in node.open_legs:
+                open_qns.append(node.qn[leg_ind])
         return open_qns
 
 def num_nonzero(quantum_numbers: list[ndarray]) -> int:
