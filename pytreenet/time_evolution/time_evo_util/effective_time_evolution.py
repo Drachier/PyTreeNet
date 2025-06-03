3830479df8f4bb031874e6dc2e00910e948f5a69
Drachier <r.milbradt@tum.de>
2025-06-03 17:04:54 +0200

    Implement effective single site and bond time evolution

--- pytreenet/time_evolution/time_evo_util/effective_time_evolution.py
+++ pytreenet/time_evolution/time_evo_util/effective_time_evolution.py
@@ -5,15 +5,25 @@ The effective Hamiltonian's are usually obtained by partially contracting a
 TTNO Hamiltonian with a TTNS and its conjugate.
 
 """
+from __future__ import annotations
+from typing import Any
 
 from numpy import ndarray
+import numpy as np
+from numpy.typing import NDArray
 
 from pytreenet.ttns import TreeTensorNetworkState
 from pytreenet.ttno import TreeTensorNetworkOperator
 from pytreenet.contractions.sandwich_caching import SandwichCache
-from pytreenet.contractions.effective_hamiltonians import get_effective_single_site_hamiltonian
+from pytreenet.contractions.effective_hamiltonians import (get_effective_single_site_hamiltonian_nodes,
+                                                           get_effective_bond_hamiltonian_nodes,
+                                                           get_effective_single_site_hamiltonian)
 from pytreenet.time_evolution.time_evolution import time_evolve, TimeEvoMode
+from pytreenet.core.node import Node
+from pytreenet.contractions.state_operator_contraction import (contract_ket_ham_with_envs,
+                                                               contract_bond_tensor)
 
+# TODO: Rewrite this to use the new time evolution interface
 def single_site_time_evolution(node_id: str,
                                state: TreeTensorNetworkState,
                                hamiltonian: TreeTensorNetworkOperator,
@@ -54,3 +64,125 @@ def single_site_time_evolution(node_id: str,
                                  forward=forward,
                                  mode=mode)
     return updated_tensor
+
+def effective_bond_evolution(
+        state_tensor: NDArray[np.complex128],
+        state_node: Node,
+        time_step_size: float,
+        tensor_cache: SandwichCache,
+        mode: TimeEvoMode = TimeEvoMode.FASTEST,
+        forward: bool = True,
+        **options: dict[str, Any]
+    ) -> NDArray[np.complex128]:
+    """
+    Perform the effective bond time evolution.
+
+    This function computes the time evolution of a bond in a TTNS via
+    a TTNO representing the Hamiltonian. The state tensor represents a tensor
+    on a bond in the TTNS without an equivalent tensor in the TTNO.
+
+    Args:
+        state_tensor (NDArray[np.complex128]): The tensor of the state to be
+            updated, representing a bond in the TTNS.
+        state_node (Node): The node of the state tensor.
+        time_step_size (float): The time step size.
+        tensor_cache (SandwichCache): The cache for the neighbour blocks.
+        mode (TimeEvoMode, optional): The mode of the time evolution. Defaults
+            to TimeEvoMode.FASTEST. If possible, the time evolution is
+            performed without constructing the full effective Hamiltonian.
+        forward (bool, optional): Whether to time evolve forward or backward.
+            Defaults to True.
+        **options: Additional options for the time evolution. See the
+            documentation of the `TimeEvoMode`-class for more
+            information.
+
+    """
+    if mode.action_evolvable():
+        def contraction_action(t,y):
+            """Defines the contraction to be used for the time evolution."""
+            return contract_bond_tensor(state_node, y, tensor_cache)
+        updated_tensor = mode.time_evolve_action(state_tensor,
+                                                 contraction_action,
+                                                 time_step_size,
+                                                 forward=forward,
+                                                 **options)
+        return updated_tensor
+    # Fallback to the default time evolution
+    ham_eff = get_effective_bond_hamiltonian_nodes(state_node, tensor_cache)
+    updated_tensor = mode.time_evolve(state_tensor,
+                                        ham_eff,
+                                        time_difference=time_step_size,
+                                        forward=forward,
+                                        **options
+                                        )
+    return updated_tensor
+
+def effective_single_site_evolution(
+        state_tensor: NDArray[np.complex128],
+        state_node: Node,
+        hamiltonian_tensor: NDArray[np.complex128],
+        hamiltonian_node: Node,
+        time_step_size: float,
+        tensor_cache: SandwichCache,
+        mode: TimeEvoMode = TimeEvoMode.FASTEST,
+        forward: bool = True,
+        **options: dict[str, Any]
+    ) -> NDArray[np.complex128]:
+    """
+    Perform the effective single site time evolution.
+
+    This function computes the time evolution of a single site in a TTNS via
+    a TTNO representing the Hamiltonian. The effective Hamiltonian is
+    defined by the hamiltonian tensor and the environment tensors in the
+    cache.
+
+    Args:
+        state_tensor (NDArray[np.complex128]): The tensor of the state to be
+            updated.
+        state_node (Node): The node of the state tensor.
+        hamiltonian_tensor (NDArray[np.complex128]): The tensor of the
+            Hamiltonian.
+        hamiltonian_node (Node): The node of the Hamiltonian tensor.
+        time_step_size (float): The time step size.
+        tensor_cache (SandwichCache): The cache for the neighbour blocks.
+        mode (TimeEvoMode, optional): The mode of the time evolution. Defaults
+            to TimeEvoMode.FASTEST. If possible, the time evolution is
+            performed without constructing the full effective Hamiltonian.
+        forward (bool, optional): Whether to time evolve forward or backward.
+            Defaults to True.
+        **options: Additional options for the time evolution. See the
+            documentation of the `TimeEvoMode`-class for more
+            information.
+
+    Returns:
+        NDArray[np.complex128]: The updated state tensor after the time
+            evolution.
+
+    """
+    if mode.action_evolvable():
+        def contraction_action(t,y):
+            """Defines the contraction to be used for the time evolution."""
+            return contract_ket_ham_with_envs(state_node,
+                                                y,
+                                                hamiltonian_node,
+                                                hamiltonian_tensor,
+                                                tensor_cache)
+        updated_tensor = mode.time_evolve_action(state_tensor,
+                                                 contraction_action,
+                                                 time_step_size,
+                                                 forward=forward,
+                                                 **options)
+        return updated_tensor
+    # Fallback to the default time evolution
+    ham_eff = get_effective_single_site_hamiltonian_nodes(state_node,
+                                                            hamiltonian_node,
+                                                            hamiltonian_tensor,
+                                                            tensor_cache)
+    updated_tensor = mode.time_evolve(state_tensor,
+                                        ham_eff,
+                                        time_difference=time_step_size,
+                                        forward=forward,
+                                        **options
+                                        )
+    return updated_tensor
+
