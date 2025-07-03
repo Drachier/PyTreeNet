f195c98382424bccc688f3177560bf64613c2ffe
R. Milbradt <74064217+Drachier@users.noreply.github.com>
2025-07-03 08:08:12 +0200

    Get plotting in latex style fixed

--- pytreenet/util/plotting.py
+++ pytreenet/util/plotting.py
@@ -4,7 +4,7 @@ This module contains helpful tools for plotting results.
 
 import matplotlib.pyplot as plt
 
-def set_size(width: float,
+def set_size(width: float | str,
              fraction: float = 1,
              subplots: tuple[int, int] = (1, 1)):
     """
@@ -13,7 +13,8 @@ def set_size(width: float,
     Source: https://jwalton.info/Embed-Publication-Matplotlib-Latex/
 
     Args:
-        width (float): Document width in points.
+        width (float | str): Document width in points or a string describing
+            the width.
         fraction (float, optional): Fraction of the width which you wish the
             figure to occupy
         subplots (tuple[int,int]): The number of rows and columns of subplots.
@@ -22,7 +23,11 @@ def set_size(width: float,
         fig_dim (tuple[float,float]): Dimensions of figure in inches,
     """
     # Width of figure (in pts)
-    fig_width_pt = width * fraction
+    if width == "thesis":
+        width_pt = 483.6969
+    else:
+        width_pt = width
+    fig_width_pt = width_pt * fraction
     # Convert from pt to inches
     inches_per_pt = 1 / 72.27
     # Golden ratio to set aesthetic figure height
@@ -40,7 +45,7 @@ def config_matplotlib_to_latex():
     """
     tex_fonts = {
         # Use LaTeX to write all text
-        "text.usetex": True,
+        #"text.usetex": True,
         "font.family": "serif",
         # Use 10pt font in plots, to match 10pt font in document
         "axes.labelsize": 10,
