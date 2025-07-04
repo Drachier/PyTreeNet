582a03c739d775c7756fe22bba376f2975f06834
Drachier <r.milbradt@tum.de>
2025-07-04 16:04:25 +0200

    Allow more than trivial leg when generating the trivia tensor

--- pytreenet/special_ttn/special_nodes.py
+++ pytreenet/special_ttn/special_nodes.py
@@ -2,23 +2,30 @@
 This module implements some commonly used nodes/tensors.
 """
 
-from numpy import zeros, ndarray, pad
+from numpy import zeros, ndarray
 
-def trivial_virtual_node(shape: tuple) -> ndarray:
+from ..util.ttn_exceptions import non_negativity_check
+
+def trivial_virtual_node(shape: tuple,
+                         num_triv_legs: int = 1
+                         ) -> ndarray:
     """
     Generates a trivial virtual tensor of a given shape.
 
-    A trivial virtual tensor has a last leg of dimension 1 and the first
-    element is the only non-zero element and equal to 1.
+    A trivial virtual tensor has trivial last legs of dimension 1, and the
+    first element is the only non-zero element and equal to 1.
 
     Args:
         shape (tuple): The shape of the tensor.
+        num_triv_legs (int): The number of trivial legs to add as last legs.
+            Defaults to 1.
     
     Returns:
         ndarray: The trivial virtual tensor.
     
     """
-    tensor = zeros([dim for dim in shape] + [1],
+    non_negativity_check(num_triv_legs, "number of trivial legs")
+    tensor = zeros(list(shape) + [1],
                    dtype=complex)
     zeros_indices = tuple([0 for _ in range(len(shape) + 1)])
     tensor[zeros_indices] = 1
