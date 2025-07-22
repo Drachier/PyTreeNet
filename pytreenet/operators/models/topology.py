3b19882ccc42d4b829a31e75c8c22b8bf033df6d
R. Milbradt <74064217+Drachier@users.noreply.github.com>
2025-07-22 13:48:40 +0200

    Implement num_sites from system size via topology

--- pytreenet/operators/models/topology.py
+++ pytreenet/operators/models/topology.py
@@ -14,3 +14,24 @@ class Topology(Enum):
     TTOPOLOGY = "t_topology"
     CALEY = "caley_tree"
     SQUARE = "square_lattice"
+
+    def num_sites(self,
+                  system_size: int) -> int:
+        """
+        Returns the number of sites in the topology.
+
+        Args:
+            system_size (int): The characteristic size of the system.
+        
+        Returns:
+            int: The number of sites in the topology.
+        """
+        if self == Topology.CHAIN:
+            return system_size
+        if self == Topology.TTOPOLOGY:
+            return system_size * 3
+        if self == Topology.CALEY:
+            return sum(2 ** i for i in range(system_size))
+        if self == Topology.SQUARE:
+            return system_size ** 2
+        raise ValueError(f"Unknown topology: {self}")
