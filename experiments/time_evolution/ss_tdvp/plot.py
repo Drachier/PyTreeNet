32a74bedf03aa4508cca6477e8e3975e6ad27d4a
Drachier <r.milbradt@tum.de>
2026-04-15 16:28:25 +0200

    Add plotting for dt vs error

--- experiments/time_evolution/ss_tdvp/plot.py
+++ experiments/time_evolution/ss_tdvp/plot.py
@@ -5,7 +5,7 @@ import os
 import sys
 
 import matplotlib.pyplot as plt
-
+from h5py import File
 import numpy.typing as npt
 
 from pytreenet.util.plotting.plotables.standard_plottable import StandardPlottable
@@ -31,7 +31,7 @@ def extract_data_and_time(res: Results,
     times = res.times()[1:]
     return times, errs
 
-def cretae_line_config(order: Order) -> LineConfig:
+def create_line_config(order: Order) -> LineConfig:
     """
     Creates a line config for the given order.
     """
@@ -60,7 +60,7 @@ def load_data_err_t(md_filter: MetadataFilter,
             ord_std_pltbs.append(pltb)
         conv_pltb = ConvergingPlottable.from_multiple_standards(ord_std_pltbs,
                                                                 conv_param="bond_dim")
-        conv_pltb.line_config = cretae_line_config(order)
+        conv_pltb.line_config = create_line_config(order)
         out.append(conv_pltb)
     return tuple(out)
 
@@ -99,8 +99,9 @@ def load_bd_vs_rt(md_filter: MetadataFilter,
         md_filter.change_criterium("order", order.value)
         desired = ["bond_dim", "simulation_time"]
         attrs = md_filter.load_valid_attributes(directory_path,
-                                                desired_keys=desired)
-        line_config = cretae_line_config(order)
+                                                desired_keys=desired,
+                                                allow_non_exist=True)
+        line_config = create_line_config(order)
         std_pltb = StandardPlottable.create_empty(line_config=line_config)
         for attr in attrs:
             std_pltb.add_point(attr[desired[0]], attr[desired[1]])
@@ -118,7 +119,7 @@ def plot_bd_vs_rt(md_filter: MetadataFilter,
     config_matplotlib_to_latex(doc_style)
     size = set_size(doc_style)
     std_pltbs = load_bd_vs_rt(md_filter, directory_path)
-    
+
     fig, ax = plt.subplots(figsize=size)
     for std_pltb in std_pltbs:
         std_pltb.plot_on_axis(ax)
@@ -131,6 +132,61 @@ def plot_bd_vs_rt(md_filter: MetadataFilter,
     save_path = os.path.join(save_path, "bd_vs_rt.pdf")
     plt.savefig(save_path, bbox_inches="tight", format="pdf")
 
+def _custom_loader_dt_finalerror(file: File) -> tuple[float, float]:
+    """
+    Custom loader function to extract the time step size and final error from
+    the HDF5 file.
+    """
+    dt = file.attrs["time_step_size"]
+    error = file["error"][-1]
+    return dt, error
+
+def load_dt_vs_final_error(md_filter: MetadataFilter,
+                           directory_path: str
+                           ) -> tuple[StandardPlottable, StandardPlottable]:
+    """
+    Loads the data for the time step size vs. final error plottables for
+    the 1TDVP simulation results.
+    """
+    out = []
+    for order in Order:
+        md_filter.change_criterium("order", order.value)
+        results = md_filter.load_custom(directory_path,
+                                        _custom_loader_dt_finalerror,
+                                        allow_non_exist=True)
+        print(results)
+        line_config = create_line_config(order)
+        std_pltb = StandardPlottable.create_empty(line_config=line_config)
+        for dt, error in results:
+            std_pltb.add_point(dt, error)
+        std_pltb.sort_by_x()
+        out.append(std_pltb)
+    return tuple(out)
+
+def plot_dt_vs_final_error(md_filter: MetadataFilter,
+                           directory_path: str
+                           ) -> None:
+    """
+    Plots the time step size vs. final error for the TDVP simulation results.
+    """
+    doc_style = DocumentStyle.THESIS
+    config_matplotlib_to_latex(doc_style)
+    size = set_size(doc_style)
+    std_pltbs = load_dt_vs_final_error(md_filter, directory_path)
+
+    fig, ax = plt.subplots(figsize=size)
+    for std_pltb in std_pltbs:
+        std_pltb.plot_on_axis(ax)
+    ax.set_xlabel("Time Step Size")
+    ax.set_ylabel("Final Error")
+    ax.set_xscale("log")
+    ax.set_yscale("log")
+    ax.legend()
+
+    save_path = os.path.join(directory_path, "plots")
+    save_path = os.path.join(save_path, "dt_vs_final_error.pdf")
+    plt.savefig(save_path, bbox_inches="tight", format="pdf")
+
 if __name__ == "__main__":
     if len(sys.argv) < 2:
         print("Usage: python plot.py <data_directory>")
@@ -139,9 +195,12 @@ if __name__ == "__main__":
     data_dir = sys.argv[1]
     
     md_filter = MetadataFilter()
-    md_filter.change_criterium("structure", TTNStructure.MPS.value)
-    md_filter.change_criterium("system_size", 12)
+    md_filter.change_criterium("structure", TTNStructure.TSTAR.value)
+    md_filter.change_criterium("system_size", 3)
     md_filter.change_criterium("ext_magn", 0.5)
     md_filter.change_criterium("time_step_size", 0.01)
     plot_err_t(md_filter, data_dir)
     plot_bd_vs_rt(md_filter, data_dir)
+    md_filter.remove_criterium("time_step_size")
+    md_filter.change_criterium("bond_dim", 64)
+    plot_dt_vs_final_error(md_filter, data_dir)
